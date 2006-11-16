f093e1f5c2cfe3085eeb333fbb7306a8297e87ba
Pierre-Arnaud Marcelot <pamarcelot@apache.org>
2006-11-16 16:51:36 +0000

    Importing project ldapstudio-dsml-plugin
    git-svn-id: https://svn.apache.org/repos/asf/directory/sandbox/pamarcelot/ldapstudio@475810 13f79535-47bb-0310-9956-ffa450edef68

---
+++ ldapstudio-dsml-plugin/src/main/java/org/apache/directory/ldapstudio/dsmlv2/controller/actions/ExecuteAction.java
@@ -0,0 +1,69 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *  
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License. 
+ *  
+ */
+
+package org.apache.directory.ldapstudio.dsmlv2.controller.actions;
+
+
+import org.apache.directory.ldapstudio.dsmlv2.view.ExecuteWizard;
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.StructuredSelection;
+import org.eclipse.jface.wizard.WizardDialog;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.IWorkbenchWindowActionDelegate;
+import org.eclipse.ui.PlatformUI;
+
+
+/**
+ * This class implements the Execute Action
+ */
+public class ExecuteAction implements IWorkbenchWindowActionDelegate
+{
+    /**
+     * This method is run when the menu item is clicked
+     */
+    public void run( IAction action )
+    {
+        ExecuteWizard wizard = new ExecuteWizard();
+        wizard.init( PlatformUI.getWorkbench(), StructuredSelection.EMPTY );
+        // Instantiates the wizard container with the wizard and opens it
+        WizardDialog dialog = new WizardDialog( PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell(), wizard );
+        dialog.create();
+        dialog.open();
+    }
+
+
+    public void dispose()
+    {
+        // This method does nothing, but is needed by the IWorkbenchWindowActionDelegate Interface
+    }
+
+
+    public void init( IWorkbenchWindow window )
+    {
+        // This method does nothing, but is needed by the IWorkbenchWindowActionDelegate Interface
+    }
+
+
+    public void selectionChanged( IAction action, ISelection selection )
+    {
+        // This method does nothing, but is needed by the IWorkbenchWindowActionDelegate Interface
+    }
+}
