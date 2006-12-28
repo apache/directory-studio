ef177ac3456d55cb1d4890e8c1eba8735c6a519e
Emmanuel Lecharny <elecharny@apache.org>
2006-12-28 16:28:21 +0000

    Removed warnings
    git-svn-id: https://svn.apache.org/repos/asf/directory/trunks/ldapstudio@490755 13f79535-47bb-0310-9956-ffa450edef68

--- ldapstudio-browser-core/src/main/java/org/apache/directory/ldapstudio/browser/core/SearchManager.java
+++ ldapstudio-browser-core/src/main/java/org/apache/directory/ldapstudio/browser/core/SearchManager.java
@@ -23,13 +23,13 @@ package org.apache.directory.ldapstudio.browser.core;
 
 import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 import org.apache.directory.ldapstudio.browser.core.events.EventRegistry;
 import org.apache.directory.ldapstudio.browser.core.events.SearchUpdateEvent;
 import org.apache.directory.ldapstudio.browser.core.model.IConnection;
 import org.apache.directory.ldapstudio.browser.core.model.ISearch;
+import org.eclipse.osgi.util.NLS;
 
 
 public class SearchManager implements Serializable
@@ -37,7 +37,7 @@ public class SearchManager implements Serializable
 
     private static final long serialVersionUID = 8665227628274097691L;
 
-    private List searchList;
+    private List<ISearch> searchList;
 
     private IConnection connection;
 
@@ -50,7 +50,7 @@ public class SearchManager implements Serializable
     public SearchManager( IConnection connection )
     {
         this.connection = connection;
-        this.searchList = new ArrayList();
+        this.searchList = new ArrayList<ISearch>();
     }
 
 
@@ -68,44 +68,46 @@ public class SearchManager implements Serializable
 
     public void addSearch( int index, ISearch search )
     {
-        if ( this.getSearch( search.getName() ) != null )
+        if ( getSearch( search.getName() ) != null )
         {
-            String newSearchName = BrowserCoreMessages.bind( BrowserCoreMessages.copy_n_of_s, "", search.getName() ); //$NON-NLS-1$
+            String newSearchName = NLS.bind( BrowserCoreMessages.copy_n_of_s, "", search.getName() ); //$NON-NLS-1$
+            
             for ( int i = 2; this.getSearch( newSearchName ) != null; i++ )
             {
-                newSearchName = BrowserCoreMessages.bind( BrowserCoreMessages.copy_n_of_s, i + " ", search.getName() ); //$NON-NLS-1$
+                newSearchName = NLS.bind( BrowserCoreMessages.copy_n_of_s, i + " ", search.getName() ); //$NON-NLS-1$
             }
+            
             search.setName( newSearchName );
         }
 
-        this.searchList.add( index, search );
+        searchList.add( index, search );
         EventRegistry.fireSearchUpdated( new SearchUpdateEvent( search, SearchUpdateEvent.SEARCH_ADDED ), this );
     }
 
 
     public ISearch getSearch( String name )
     {
-        for ( Iterator it = this.searchList.iterator(); it.hasNext(); )
+        for ( ISearch search:searchList )
         {
-            ISearch search = ( ISearch ) it.next();
             if ( search.getName().equals( name ) )
             {
                 return search;
             }
         }
+        
         return null;
     }
 
 
     public int indexOf( ISearch search )
     {
-        return this.searchList.indexOf( search );
+        return searchList.indexOf( search );
     }
 
 
     public void removeSearch( ISearch search )
     {
-        this.searchList.remove( search );
+        searchList.remove( search );
         EventRegistry.fireSearchUpdated( new SearchUpdateEvent( search, SearchUpdateEvent.SEARCH_REMOVED ), this );
     }
 
@@ -118,13 +120,13 @@ public class SearchManager implements Serializable
 
     public ISearch[] getSearches()
     {
-        return ( ISearch[] ) this.searchList.toArray( new ISearch[0] );
+        return searchList.toArray( new ISearch[0] );
     }
 
 
     public int getSearchCount()
     {
-        return this.searchList.size();
+        return searchList.size();
     }
 
 }
