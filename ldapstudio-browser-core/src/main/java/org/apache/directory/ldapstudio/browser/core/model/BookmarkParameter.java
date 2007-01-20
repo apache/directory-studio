2f763293a3306471693be728320f1b5eddc98536
Stefan Seelmann <seelmann@apache.org>
2007-01-20 23:58:37 +0000

    Added Javadocs, code cleaning.
    git-svn-id: https://svn.apache.org/repos/asf/directory/trunks/ldapstudio@498214 13f79535-47bb-0310-9956-ffa450edef68

--- ldapstudio-browser-core/src/main/java/org/apache/directory/ldapstudio/browser/core/model/BookmarkParameter.java
+++ ldapstudio-browser-core/src/main/java/org/apache/directory/ldapstudio/browser/core/model/BookmarkParameter.java
@@ -24,21 +24,40 @@ package org.apache.directory.ldapstudio.browser.core.model;
 import java.io.Serializable;
 
 
+/**
+ * A Bean class to hold the bookmark parameters. 
+ * It is used to make bookmarks persistent.
+ *
+ * @author <a href="mailto:dev@directory.apache.org">Apache Directory Project</a>
+ * @version $Rev$, $Date$
+ */
 public class BookmarkParameter implements Serializable
 {
 
+    /** The serialVersionUID. */
     private static final long serialVersionUID = 105108281861642267L;
 
+    /** The target DN. */
     private DN dn;
 
+    /** The symbolic name. */
     private String name;
 
 
+    /**
+     * Creates a new instance of BookmarkParameter.
+     */
     public BookmarkParameter()
     {
     }
 
 
+    /**
+     * Creates a new instance of BookmarkParameter.
+     *
+     * @param dn the target DN
+     * @param name the symbolic name
+     */
     public BookmarkParameter( DN dn, String name )
     {
         this.dn = dn;
@@ -46,24 +65,44 @@ public class BookmarkParameter implements Serializable
     }
 
 
+    /**
+     * Gets the target DN.
+     * 
+     * @return the target DN
+     */
     public DN getDn()
     {
         return dn;
     }
 
 
+    /**
+     * Sets the target DN.
+     * 
+     * @param dn the target DN
+     */
     public void setDn( DN dn )
     {
         this.dn = dn;
     }
 
 
+    /**
+     * Gets the symbolic name.
+     * 
+     * @return the symbolic name
+     */
     public String getName()
     {
         return name;
     }
 
 
+    /**
+     * Sets the symbolic name.
+     * 
+     * @param name the symbolic name
+     */
     public void setName( String name )
     {
         this.name = name;
--- ldapstudio-browser-core/src/main/java/org/apache/directory/ldapstudio/browser/core/model/SearchParameter.java
+++ ldapstudio-browser-core/src/main/java/org/apache/directory/ldapstudio/browser/core/model/SearchParameter.java
@@ -23,86 +23,169 @@ package org.apache.directory.ldapstudio.browser.core.model;
 
 import java.io.Serializable;
 
-import org.apache.directory.ldapstudio.browser.core.utils.LdifUtils;
 
-
+/**
+ * A Bean class to hold the search parameters.
+ * It is used to make searches persistent.
+ * 
+ * TODO: check problems with null searchBase, filter and returningAttributes!
+ * 
+ * @author <a href="mailto:dev@directory.apache.org">Apache Directory Project</a>
+ * @version $Rev$, $Date$
+ */
 public class SearchParameter implements Serializable
 {
 
+    /** The serialVersionUID. */
     private static final long serialVersionUID = 2447490121520960805L;
 
+    /** The symbolic name. */
     private String name;
 
+    /** The search base. */
     private DN searchBase;
 
-    private String filter = ISearch.FILTER_TRUE;
+    /** The filter. */
+    private String filter;
 
-    private String[] returningAttributes = null;
+    /** The returning attributes. */
+    private String[] returningAttributes;
 
-    private int scope = ISearch.SCOPE_OBJECT;
+    /** The search scope, one of ISearch.SCOPE_OBJECT, ISearch.SCOPE_ONELEVEL or ISearch.SCOPE_SUBTREE. */
+    private int scope;
 
-    private int timeLimit = 0;
+    /** The time limit in milliseconds, 0 means no limit. */
+    private int timeLimit;
 
-    private int countLimit = 0;
+    /** The count limit, 0 means no limit. */
+    private int countLimit;
 
-    private int aliasesDereferencingMethod = IConnection.DEREFERENCE_ALIASES_NEVER;
+    /** The alias dereferencing method, one of IConnection.DEREFERENCE_ALIASES_NEVER, IConnection.DEREFERENCE_ALIASES_ALWAYS, IConnection.DEREFERENCE_ALIASES_FINDING or IConnection.DEREFERENCE_ALIASES_SEARCH. */
+    private int aliasesDereferencingMethod;
 
-    private int referralsHandlingMethod = IConnection.HANDLE_REFERRALS_IGNORE;
+    /** The referrals handling method, one of IConnection.HANDLE_REFERRALS_IGNORE or IConnection.HANDLE_REFERRALS_FOLLOW. */
+    private int referralsHandlingMethod;
 
-    private Control[] controls = null;
+    /** The controls */
+    private Control[] controls;
 
-    private boolean initChildrenFlag = false;
+    /** Flag indicating wether the hasChildren flag of IEntry should be initialized */
+    private boolean initChildrenFlag;
 
-    private boolean initAliasAndReferralFlag = false;
+    /** Flag indicating wether the isAlias and isReferral flag of IEntry should be initialized */
+    private boolean initAliasAndReferralFlag;
 
 
+    /**
+     * Creates a new instance of SearchParameter with default search parameters:
+     * <ul>
+     * <li>Filter (objectClass=*)
+     * <li>Object scope
+     * <li>No time limit
+     * <li<No count limit
+     * <li>Never dereference aliases
+     * <li>Ignore referrals
+     * <li>No controls
+     * <li>Don't initialize hasChildren flag
+     * <li>Don't initialize isAlias and isReferral flag
+     * </ul>
+     */
     public SearchParameter()
     {
+        filter = ISearch.FILTER_TRUE;
+        scope = ISearch.SCOPE_OBJECT;
+        timeLimit = 0;
+        countLimit = 0;
+        aliasesDereferencingMethod = IConnection.DEREFERENCE_ALIASES_NEVER;
+        referralsHandlingMethod = IConnection.HANDLE_REFERRALS_IGNORE;
+        controls = null;
+        initChildrenFlag = false;
+        initAliasAndReferralFlag = false;
     }
 
 
+    /**
+     * Gets the count limit, 0 means no limit.
+     * 
+     * @return the count limit
+     */
     public int getCountLimit()
     {
         return countLimit;
     }
 
 
+    /**
+     * Sets the count limit, 0 means no limit.
+     * 
+     * @param countLimit the count limit
+     */
     public void setCountLimit( int countLimit )
     {
         this.countLimit = countLimit;
     }
 
 
+    /**
+     * Gets the filter.
+     * 
+     * @return the filter
+     */
     public String getFilter()
     {
         return filter;
     }
 
 
+    /**
+     * Sets the filter.
+     * 
+     * @param filter the filter
+     */
     public void setFilter( String filter )
     {
         this.filter = filter;
     }
 
 
+    /**
+     * Gets the symbolic name.
+     * 
+     * @return the name
+     */
     public String getName()
     {
         return name;
     }
 
 
+    /**
+     * Sets the symbolic name.
+     * 
+     * @param name the name
+     */
     public void setName( String name )
     {
         this.name = name;
     }
 
 
+    /**
+     * Gets the returning attributes.
+     * 
+     * @return the returning attributes
+     */
     public String[] getReturningAttributes()
     {
         return returningAttributes;
     }
 
 
+    /**
+     * Sets the returning attributes.
+     * 
+     * @param returningAttributes the returning attributes
+     */
     public void setReturningAttributes( String[] returningAttributes )
     {
         if ( returningAttributes == null )
@@ -115,66 +198,127 @@ public class SearchParameter implements Serializable
     }
 
 
+    /**
+     * Gets the scope, one of ISearch.SCOPE_OBJECT, 
+     * ISearch.SCOPE_ONELEVEL or ISearch.SCOPE_SUBTREE.
+     * 
+     * @return the scope
+     */
     public int getScope()
     {
         return scope;
     }
 
 
+    /**
+     * Sets the scope, one of ISearch.SCOPE_OBJECT, 
+     * ISearch.SCOPE_ONELEVEL or ISearch.SCOPE_SUBTREE.
+     * 
+     * @param scope the scope
+     */
     public void setScope( int scope )
     {
         this.scope = scope;
     }
 
 
+    /**
+     * Gets the aliases dereferencing method, one of IConnection.DEREFERENCE_ALIASES_NEVER, 
+     * IConnection.DEREFERENCE_ALIASES_ALWAYS, IConnection.DEREFERENCE_ALIASES_FINDING 
+     * or IConnection.DEREFERENCE_ALIASES_SEARCH.
+     * 
+     * @return the aliases dereferencing method
+     */
     public int getAliasesDereferencingMethod()
     {
         return aliasesDereferencingMethod;
     }
 
 
+    /**
+     * Sets the aliases dereferencing method, one of IConnection.DEREFERENCE_ALIASES_NEVER, 
+     * IConnection.DEREFERENCE_ALIASES_ALWAYS, IConnection.DEREFERENCE_ALIASES_FINDING 
+     * or IConnection.DEREFERENCE_ALIASES_SEARCH.
+     * 
+     * @param aliasesDereferencingMethod the aliases dereferencing method
+     */
     public void setAliasesDereferencingMethod( int aliasesDereferencingMethod )
     {
         this.aliasesDereferencingMethod = aliasesDereferencingMethod;
     }
 
 
+    /**
+     * Gets the referrals handling method, one of IConnection.HANDLE_REFERRALS_IGNORE
+     *  or IConnection.HANDLE_REFERRALS_FOLLOW.
+     * 
+     * @return the referrals handling method
+     */
     public int getReferralsHandlingMethod()
     {
         return referralsHandlingMethod;
     }
 
 
+    /**
+     * Sets the referrals handling method, one of IConnection.HANDLE_REFERRALS_IGNORE or 
+     * IConnection.HANDLE_REFERRALS_FOLLOW.
+     * 
+     * @param referralsHandlingMethod the referrals handling method
+     */
     public void setReferralsHandlingMethod( int referralsHandlingMethod )
     {
         this.referralsHandlingMethod = referralsHandlingMethod;
     }
 
 
+    /**
+     * Gets the search base.
+     * 
+     * @return the search base
+     */
     public DN getSearchBase()
     {
         return searchBase;
     }
 
 
+    /**
+     * Sets the search base.
+     * 
+     * @param searchBase the search base
+     */
     public void setSearchBase( DN searchBase )
     {
         this.searchBase = searchBase;
     }
 
 
+    /**
+     * Gets the time limit in milliseconds, 0 means no limit.
+     * 
+     * @return the time limit
+     */
     public int getTimeLimit()
     {
         return timeLimit;
     }
 
 
+    /**
+     * Sets the time limit in milliseconds, 0 means no limit.
+     * 
+     * @param timeLimit the time limit
+     */
     public void setTimeLimit( int timeLimit )
     {
         this.timeLimit = timeLimit;
     }
 
 
+    /**
+     * {@inheritDoc}
+     */
     public Object clone()
     {
         SearchParameter clone = new SearchParameter();
@@ -194,78 +338,66 @@ public class SearchParameter implements Serializable
     }
 
 
-    public String getURL()
-    {
-        // ldap://host:port/dn?attributes?scope?filter?extensions
-        StringBuffer sb = new StringBuffer();
-
-        sb.append( LdifUtils.urlEncode( searchBase.toString() ) );
-
-        sb.append( '?' );
-
-        for ( int i = 0; returningAttributes != null && i < returningAttributes.length; i++ )
-        {
-            sb.append( LdifUtils.urlEncode( returningAttributes[i] ) );
-            if ( i + 1 < returningAttributes.length )
-                sb.append( ',' );
-        }
-
-        sb.append( '?' );
-
-        if ( scope == ISearch.SCOPE_OBJECT )
-        {
-            sb.append( "base" ); //$NON-NLS-1$
-        }
-        else if ( scope == ISearch.SCOPE_ONELEVEL )
-        {
-            sb.append( "one" ); //$NON-NLS-1$
-        }
-        else if ( scope == ISearch.SCOPE_SUBTREE )
-        {
-            sb.append( "sub" ); //$NON-NLS-1$
-        }
-
-        sb.append( '?' );
-
-        if ( filter != null )
-        {
-            sb.append( LdifUtils.urlEncode( filter ) );
-        }
-
-        return sb.toString();
-    }
-
-
+    /**
+     * Checks if the hasChildren flag of IEntry should be initialized.
+     * 
+     * @return true, if the hasChildren flag of IEntry should be initialized
+     */
     public boolean isInitAliasAndReferralFlag()
     {
         return initAliasAndReferralFlag;
     }
 
 
+    /**
+     * Sets if the hasChildren flag of IEntry should be initialized.
+     * 
+     * @param initAliasAndReferralFlag the init alias and referral flag
+     */
     public void setInitAliasAndReferralFlag( boolean initAliasAndReferralFlag )
     {
         this.initAliasAndReferralFlag = initAliasAndReferralFlag;
     }
 
 
+    /**
+     * Checks if the hasChildren flag of IEntry should be initialized.
+     * 
+     * @return true, if the hasChildren flag of IEntry should be initialized
+     */
     public boolean isInitChildrenFlag()
     {
         return initChildrenFlag;
     }
 
 
+    /**
+     * Sets if the hasChildren flag of IEntry should be initialized.
+     * 
+     * @param initChildrenFlag the init children flag
+     */
     public void setInitChildrenFlag( boolean initChildrenFlag )
     {
         this.initChildrenFlag = initChildrenFlag;
     }
 
 
+    /**
+     * Gets the controls.
+     * 
+     * @return the controls
+     */
     public Control[] getControls()
     {
         return controls;
     }
 
 
+    /**
+     * Sets the controls.
+     * 
+     * @param controls the controls
+     */
     public void setControls( Control[] controls )
     {
         this.controls = controls;
