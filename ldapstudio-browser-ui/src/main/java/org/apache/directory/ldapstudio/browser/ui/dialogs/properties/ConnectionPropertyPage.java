e1a0161d7ac4aba8c59563beebbc6a082b0004f2
Pierre-Arnaud Marcelot <pamarcelot@apache.org>
2007-01-26 10:12:16 +0000

    Fixed DIRSTUDIO-18 (Password field disabled when switching from Anonymous to Simple Authentication).
    git-svn-id: https://svn.apache.org/repos/asf/directory/ldapstudio/trunk@500206 13f79535-47bb-0310-9956-ffa450edef68

--- ldapstudio-browser-ui/src/main/java/org/apache/directory/ldapstudio/browser/ui/dialogs/properties/ConnectionPropertyPage.java
+++ ldapstudio-browser-ui/src/main/java/org/apache/directory/ldapstudio/browser/ui/dialogs/properties/ConnectionPropertyPage.java
@@ -132,7 +132,7 @@ public class ConnectionPropertyPage extends PropertyPage implements ConnectionPa
         gl = new GridLayout( 1, false );
         authComposite.setLayout( gl );
         cpw.addAuthenticationMethodInput( connection.getAuthMethod(), authComposite );
-        cpw.addSimpleAuthInput( connection.getBindPassword() != null,
+        cpw.addSimpleAuthInput( ( connection.getBindPassword() != null ) || ( connection.getBindPrincipal() == null && connection.getBindPassword() == null ),
             connection.getBindPrincipal() != null ? connection.getBindPrincipal().toString() : "", connection
                 .getBindPassword() != null ? connection.getBindPassword() : "", authComposite );
         this.authTab = new TabItem( this.tabFolder, SWT.NONE );
