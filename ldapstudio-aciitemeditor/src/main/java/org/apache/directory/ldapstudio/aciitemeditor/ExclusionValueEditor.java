a020d82948d8b56909ed737b9a5cc72efc647cdb
Pierre-Arnaud Marcelot <pamarcelot@apache.org>
2007-04-03 16:30:37 +0000

    Updated the SubtreeSpecification Value Editor and the Exclusion Value Editor. updated exported pacakges in the ldapstudio-browser-ui project.
    git-svn-id: https://svn.apache.org/repos/asf/directory/ldapstudio/trunk@525202 13f79535-47bb-0310-9956-ffa450edef68

--- ldapstudio-aciitemeditor/src/main/java/org/apache/directory/ldapstudio/aciitemeditor/ExclusionValueEditor.java
+++ ldapstudio-aciitemeditor/src/main/java/org/apache/directory/ldapstudio/aciitemeditor/ExclusionValueEditor.java
@@ -23,9 +23,13 @@ package org.apache.directory.ldapstudio.aciitemeditor;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import org.apache.directory.ldapstudio.browser.core.model.DN;
 import org.apache.directory.ldapstudio.browser.core.model.IConnection;
+import org.apache.directory.ldapstudio.browser.core.model.NameException;
+import org.apache.directory.ldapstudio.browser.ui.dialogs.TextDialog;
 import org.apache.directory.ldapstudio.browser.ui.valueeditors.AbstractDialogStringValueEditor;
 import org.apache.directory.ldapstudio.browser.ui.widgets.BaseWidgetUtils;
+import org.apache.directory.ldapstudio.browser.ui.widgets.search.EntryWidget;
 import org.eclipse.jface.dialogs.Dialog;
 import org.eclipse.jface.dialogs.IDialogConstants;
 import org.eclipse.jface.viewers.ArrayContentProvider;
@@ -42,35 +46,35 @@ import org.eclipse.swt.widgets.Shell;
 
 
 /**
- * TODO ExclusionValueEditor.
+ * ACI item editor specific value editor to edit an Exclusion.
  *
  * @author <a href="mailto:dev@directory.apache.org">Apache Directory Project</a>
  * @version $Rev$, $Date$
  */
 public class ExclusionValueEditor extends AbstractDialogStringValueEditor
 {
-
     /* (non-Javadoc)
      * @see org.apache.directory.ldapstudio.browser.ui.valueeditors.AbstractDialogValueEditor#openDialog(org.eclipse.swt.widgets.Shell)
      */
-    @Override
     protected boolean openDialog( Shell shell )
     {
         Object value = getValue();
         if ( value != null && value instanceof ExclusionWrapper )
         {
-            ExclusionWrapper wrapper = ( ExclusionWrapper ) value;
-            ExclusionDialog dialog = new ExclusionDialog( shell, wrapper.type, null );
-            dialog.open();
+            ExclusionDialog dialog = new ExclusionDialog( shell, ( ExclusionWrapper ) value );
+            if ( dialog.open() == TextDialog.OK && !"".equals( dialog.getType() ) && !"".equals( dialog.getRDN() ) )
+            {
+                setValue( dialog.getType() + ": \"" + dialog.getRDN() + "\"" );
+                return true;
+            }
         }
+
         return false;
     }
 
 
-    /**
-     * {@inheritDoc}
-     * 
-     * Returns an MaxValueCountValueEditorRawValueWrapper.
+    /* (non-Javadoc)
+     * @see org.apache.directory.ldapstudio.browser.ui.valueeditors.AbstractDialogStringValueEditor#getRawValue(org.apache.directory.ldapstudio.browser.core.model.IConnection, java.lang.Object)
      */
     public Object getRawValue( IConnection connection, Object value )
     {
@@ -85,7 +89,7 @@ public class ExclusionValueEditor extends AbstractDialogStringValueEditor
         try
         {
             // for example: chopAfter: "ou=A"
-            Pattern pattern = Pattern.compile( "\\s*([chopBefore|chopAfter]):\\s*\"(.*)\"\\s*" );
+            Pattern pattern = Pattern.compile( "\\s*(chopBefore|chopAfter):\\s*\"(.*)\"\\s*" );
             Matcher matcher = pattern.matcher( stringValue );
             type = matcher.matches() ? matcher.group( 1 ) : "";
             rdn = matcher.matches() ? matcher.group( 2 ) : "";
@@ -94,19 +98,21 @@ public class ExclusionValueEditor extends AbstractDialogStringValueEditor
         {
         }
 
-        ExclusionWrapper wrapper = new ExclusionWrapper( type, rdn );
+        ExclusionWrapper wrapper = new ExclusionWrapper( connection, type, rdn );
         return wrapper;
     }
 
     /**
-     * The ExclusionWrapper is used to pass contextual 
-     * information to the opened ExclusionDialog.
+     * The ExclusionWrapper is used to pass contextual information to the opened ExclusionDialog.
      *
      * @author <a href="mailto:dev@directory.apache.org">Apache Directory Project</a>
      * @version $Rev$, $Date$
      */
     private class ExclusionWrapper
     {
+        /** The connection */
+        private IConnection connection;
+
         /** The type, used as initial type. */
         private String type;
 
@@ -122,15 +128,16 @@ public class ExclusionValueEditor extends AbstractDialogStringValueEditor
          * @param rdn
          *      the rdn
          */
-        private ExclusionWrapper( String type, String rdn )
+        private ExclusionWrapper( IConnection connection, String type, String rdn )
         {
+            this.connection = connection;
             this.type = type;
             this.rdn = rdn;
         }
     }
 
     /**
-     * TODO ExclusionDialog.
+     * This class provides a dialog to enter the Exclusion values.
      *
      * @author <a href="mailto:dev@directory.apache.org">Apache Directory Project</a>
      * @version $Rev$, $Date$
@@ -140,12 +147,6 @@ public class ExclusionValueEditor extends AbstractDialogStringValueEditor
         /** The dialog title */
         public static final String DIALOG_TITLE = "Exclusion Editor";
 
-        /** The initial type */
-        private String initialType;
-
-        /** The initial RDN */
-        private String initialRDN;
-
         /** The return type */
         private String returnType;
 
@@ -153,6 +154,12 @@ public class ExclusionValueEditor extends AbstractDialogStringValueEditor
         private String returnRDN;
 
         private Combo typeCombo;
+
+        private ExclusionWrapper wrapper;
+
+        private ComboViewer typeComboViewer;
+
+        private EntryWidget entryWidget;
 
         private static final String CHOP_BEFORE = "chopBefore";
 
@@ -164,11 +171,10 @@ public class ExclusionValueEditor extends AbstractDialogStringValueEditor
          *
          * @param parentShell
          */
-        protected ExclusionDialog( Shell parentShell, String initialType, String initialRDN )
+        protected ExclusionDialog( Shell parentShell, ExclusionWrapper wrapper )
         {
             super( parentShell );
-            this.initialType = initialType;
-            this.initialRDN = initialRDN;
+            this.wrapper = wrapper;
         }
 
 
@@ -182,13 +188,13 @@ public class ExclusionValueEditor extends AbstractDialogStringValueEditor
         }
 
 
-        /**
-         * {@inheritDoc}
+        /* (non-Javadoc)
+         * @see org.eclipse.jface.dialogs.Dialog#okPressed()
          */
         protected void okPressed()
         {
             returnType = typeCombo.getText();
-            // initialRDN = rdnFields.getSelection();
+            returnRDN = entryWidget.getDn().toString();
             super.okPressed();
         }
 
@@ -202,22 +208,51 @@ public class ExclusionValueEditor extends AbstractDialogStringValueEditor
             GridData gd = new GridData( GridData.FILL_BOTH );
             gd.widthHint = convertHorizontalDLUsToPixels( IDialogConstants.MINIMUM_MESSAGE_AREA_WIDTH );
             composite.setLayoutData( gd );
-            composite.setLayout( new GridLayout( 2, false ) );
+            composite.setLayout( new GridLayout( 3, false ) );
 
             BaseWidgetUtils.createLabel( composite, "Type:", 1 );
-            typeCombo = new Combo( composite, SWT.NONE );
+            typeCombo = new Combo( composite, SWT.READ_ONLY );
             String[] types = new String[2];
             types[0] = CHOP_BEFORE;
             types[1] = CHOP_AFTER;
-            ComboViewer typeComboViewer = new ComboViewer( typeCombo );
+            typeComboViewer = new ComboViewer( typeCombo );
             typeComboViewer.setContentProvider( new ArrayContentProvider() );
             typeComboViewer.setLabelProvider( new LabelProvider() );
             typeComboViewer.setInput( types );
-            typeComboViewer.setSelection( new StructuredSelection( initialType ) );
+            GridData gridData = new GridData();
+            gridData.horizontalSpan = 2;
+            gridData.grabExcessHorizontalSpace = true;
+            gridData.verticalAlignment = GridData.CENTER;
+            gridData.horizontalAlignment = GridData.BEGINNING;
+            typeCombo.setLayoutData( gridData );
 
-            BaseWidgetUtils.createLabel( composite, "RDN:", 2 );
+            BaseWidgetUtils.createLabel( composite, "RDN:", 1 );
+            entryWidget = new EntryWidget( wrapper.connection, null );
+            entryWidget.createWidget( composite );
+
+            initFromInput();
 
             return composite;
+        }
+
+
+        /**
+         * Initializes the Value Editor from the input.
+         */
+        private void initFromInput()
+        {
+            typeComboViewer.setSelection( new StructuredSelection( wrapper.type ) );
+
+            try
+            {
+                DN dn = new DN( wrapper.rdn );
+                entryWidget.setInput( wrapper.connection, dn );
+            }
+            catch ( NameException e )
+            {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
         }
 
 
--- ldapstudio-aciitemeditor/src/main/java/org/apache/directory/ldapstudio/aciitemeditor/widgets/SubtreeSpecificationValueEditor.java
+++ ldapstudio-aciitemeditor/src/main/java/org/apache/directory/ldapstudio/aciitemeditor/widgets/SubtreeSpecificationValueEditor.java
@@ -20,12 +20,23 @@
 package org.apache.directory.ldapstudio.aciitemeditor.widgets;
 
 
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
 import org.apache.directory.ldapstudio.aciitemeditor.Activator;
 import org.apache.directory.ldapstudio.aciitemeditor.ExclusionValueEditor;
-import org.apache.directory.ldapstudio.browser.core.model.IValue;
+import org.apache.directory.ldapstudio.browser.core.model.DN;
+import org.apache.directory.ldapstudio.browser.core.model.IConnection;
+import org.apache.directory.ldapstudio.browser.core.model.NameException;
 import org.apache.directory.ldapstudio.browser.ui.dialogs.TextDialog;
 import org.apache.directory.ldapstudio.browser.ui.valueeditors.AbstractDialogStringValueEditor;
 import org.apache.directory.ldapstudio.browser.ui.widgets.BaseWidgetUtils;
+import org.apache.directory.ldapstudio.browser.ui.widgets.search.EntryWidget;
+import org.apache.directory.shared.ldap.name.LdapDN;
+import org.apache.directory.shared.ldap.subtree.SubtreeSpecification;
+import org.apache.directory.shared.ldap.subtree.SubtreeSpecificationParser;
 import org.eclipse.jface.dialogs.Dialog;
 import org.eclipse.jface.dialogs.IDialogConstants;
 import org.eclipse.jface.viewers.ArrayContentProvider;
@@ -51,7 +62,7 @@ import org.eclipse.swt.widgets.Table;
 
 
 /**
- * TODO SubtreeSpecificationValueEditor.
+ * ACI item editor specific value editor to edit the SubtreeSpecification.
  *
  * @author <a href="mailto:dev@directory.apache.org">Apache Directory Project</a>
  * @version $Rev$, $Date$
@@ -63,56 +74,116 @@ public class SubtreeSpecificationValueEditor extends AbstractDialogStringValueEd
      */
     protected boolean openDialog( Shell shell )
     {
-        SubtreeDialog dialog = new SubtreeDialog( shell );
-        if ( dialog.open() == TextDialog.OK )
+        Object value = getValue();
+        if ( value != null && value instanceof SubtreeSpecificationValueWrapper )
         {
-            return true;
+            SubtreeSpecificationValueWrapper wrapper = ( SubtreeSpecificationValueWrapper ) value;
+            SubtreeSpecificationDialog dialog = new SubtreeSpecificationDialog( shell, wrapper.connection,
+                wrapper.subtreeSpecification );
+            if ( dialog.open() == TextDialog.OK )
+            {
+                return true;
+            }
         }
-
         return false;
     }
 
+
+    /* (non-Javadoc)
+     * @see org.apache.directory.ldapstudio.browser.ui.valueeditors.AbstractDialogStringValueEditor#getRawValue(org.apache.directory.ldapstudio.browser.core.model.IConnection, java.lang.Object)
+     */
+    public Object getRawValue( IConnection connection, Object value )
+    {
+        Object o = super.getRawValue( connection, value );
+        if ( o != null && o instanceof String )
+        {
+            SubtreeSpecificationParser parser = new SubtreeSpecificationParser( null );
+            try
+            {
+                SubtreeSpecification subtreeSpecification = parser.parse( ( String ) value );
+                if ( subtreeSpecification != null )
+                {
+                    return new SubtreeSpecificationValueWrapper( connection, subtreeSpecification );
+                }
+            }
+            catch ( ParseException e1 )
+            {
+                return new SubtreeSpecificationValueWrapper( connection, null );
+            }
+        }
+
+        return null;
+    }
+
     /**
-     * TODO SubtreeDialog.
+     * This class provides a dialog to enter the Subtree Specification value.
      *
      * @author <a href="mailto:dev@directory.apache.org">Apache Directory Project</a>
      * @version $Rev$, $Date$
      */
-    private class SubtreeDialog extends Dialog
+    private class SubtreeSpecificationDialog extends Dialog
     {
         /** The dialog title */
-        public static final String DIALOG_TITLE = "Subtree Editor";
+        private static final String DIALOG_TITLE = "Subtree Editor";
 
+        /** The connection */
+        private IConnection connection;
+
+        /** The SubtreeSpecification */
+        private SubtreeSpecification subtreeSpecification;
+
+        /** The Exclusions List */
+        private List<String> exclusions;
+
+        // UI Fields
+        private EntryWidget entryWidget;
         private Spinner minimumSpinner;
         private int initialMinimum = 0;
         private Spinner maximumSpinner;
         private int initialMaximum = 0;
-
         private TableViewer exclusionsTableViewer;
-
         private Button exclusionsTableAddButton;
-
         private Button exclusionsTableEditButton;
-
         private Button exclusionsTableDeleteButton;
-
         private TableViewer refinementsTableViewer;
-
         private Button refinementsTableAddButton;
-
         private Button refinementsTableEditButton;
-
         private Button refinementsTableDeleteButton;
 
 
         /**
-         * Creates a new instance of SubtreeDialog.
+         * Creates a new instance of SubtreeSpecificationDialog.
          *
-         * @param parentShell
+         * @param shell
+         *      the shell to use
+         * @param connection
+         *      the connection to use
+         * @param subtreeSpecification
+         *      the SubtreeSpecification
          */
-        protected SubtreeDialog( Shell parentShell )
+        private SubtreeSpecificationDialog( Shell shell, IConnection connection,
+            SubtreeSpecification subtreeSpecification )
         {
-            super( parentShell );
+            super( shell );
+            this.connection = connection;
+            this.subtreeSpecification = subtreeSpecification;
+            exclusions = new ArrayList<String>();
+            if ( subtreeSpecification != null )
+            {
+                Set chopBeforeExclusions = subtreeSpecification.getChopBeforeExclusions();
+                for ( Object chopBeforeExclusion : chopBeforeExclusions )
+                {
+                    LdapDN dn = ( LdapDN ) chopBeforeExclusion;
+                    exclusions.add( "chopBefore: \"" + dn.toNormName() + "\"" );
+                }
+
+                Set chopAfterExclusions = subtreeSpecification.getChopAfterExclusions();
+                for ( Object chopAfterExclusion : chopAfterExclusions )
+                {
+                    LdapDN dn = ( LdapDN ) chopAfterExclusion;
+                    exclusions.add( "chopAfter: \"" + dn.toNormName() + "\"" );
+                }
+            }
         }
 
 
@@ -137,7 +208,9 @@ public class SubtreeSpecificationValueEditor extends AbstractDialogStringValueEd
             composite.setLayoutData( gd );
             composite.setLayout( new GridLayout( 3, false ) );
 
-            BaseWidgetUtils.createLabel( composite, "Base:", 3 );
+            BaseWidgetUtils.createLabel( composite, "Base:", 1 );
+            entryWidget = new EntryWidget( connection, null );
+            entryWidget.createWidget( composite );
 
             GridData spinnersGridData = new GridData();
             spinnersGridData.grabExcessHorizontalSpace = true;
@@ -171,10 +244,43 @@ public class SubtreeSpecificationValueEditor extends AbstractDialogStringValueEd
             createRefinementsTable( composite );
 
             applyDialogFont( composite );
+
+            initFromInput();
+
             return composite;
         }
 
 
+        /**
+         * Initializes the Value Editor from the input.
+         */
+        private void initFromInput()
+        {
+            DN dn = null;
+            try
+            {
+                dn = new DN( subtreeSpecification.getBase().toNormName() );
+            }
+            catch ( NameException e )
+            {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            entryWidget.setInput( connection, dn );
+
+            minimumSpinner.setSelection( subtreeSpecification.getMinBaseDistance() );
+            maximumSpinner.setSelection( subtreeSpecification.getMaxBaseDistance() );
+
+            exclusionsTableViewer.setInput( exclusions );
+        }
+
+
+        /**
+         * Creates the Exclusions Table.
+         *
+         * @param composite
+         *      the composite
+         */
         private void createExclusionsTable( Composite composite )
         {
             GridData tableGridData = new GridData();
@@ -191,8 +297,6 @@ public class SubtreeSpecificationValueEditor extends AbstractDialogStringValueEd
             exclusionsTableViewer = new TableViewer( exclusionsTable );
             exclusionsTableViewer.setContentProvider( new ArrayContentProvider() );
             exclusionsTableViewer.setLabelProvider( new LabelProvider() );
-            exclusionsTableViewer.setInput( new String[]
-                { "chopBefore: \"ou=A\"", "chopAfter: \"ou=A\"" } );
             exclusionsTableViewer.addSelectionChangedListener( new ISelectionChangedListener()
             {
                 public void selectionChanged( SelectionChangedEvent event )
@@ -265,8 +369,8 @@ public class SubtreeSpecificationValueEditor extends AbstractDialogStringValueEd
 
 
         /**
-         * TODO valueSelectedExclusionsTable.
-         *
+         * Called when value is selected in Exclusions table viewer.
+         * Updates the enabled/disabled state of the buttons.
          */
         private void valueSelectedExclusionsTable()
         {
@@ -286,9 +390,10 @@ public class SubtreeSpecificationValueEditor extends AbstractDialogStringValueEd
 
 
         /**
-         * TODO getSelectedValue.
-         *
+         * Retuns the current selection in the Exclusions table viewer.
+         * 
          * @return
+         *      the value that is selected in the Exclusions table viewer, or null.
          */
         private String getSelectedValueExclusionsTable()
         {
@@ -309,59 +414,70 @@ public class SubtreeSpecificationValueEditor extends AbstractDialogStringValueEd
 
 
         /**
-         * TODO addValueExclusionsTable.
-         *
+         * Opens the editor and adds the new Exclusion value to the list.
          */
         private void addValueExclusionsTable()
         {
             ExclusionValueEditor valueEditor = new ExclusionValueEditor();
 
-            Object oldRawValue = valueEditor.getRawValue( null, "" );
+            Object oldRawValue = valueEditor.getRawValue( connection, "" );
 
             CellEditor cellEditor = valueEditor.getCellEditor();
             cellEditor.setValue( oldRawValue );
             cellEditor.activate();
+            Object newRawValue = cellEditor.getValue();
+
+            if ( newRawValue != null )
+            {
+                String newValue = ( String ) valueEditor.getStringOrBinaryValue( newRawValue );
+
+                exclusions.add( newValue );
+                exclusionsTableViewer.refresh();
+            }
         }
 
 
         /**
-         * TODO editValueExclusionsTable.
-         *
+         * Opens the editor with the currently selected Exclusion
+         * value and puts the modified value into the list.
          */
         private void editValueExclusionsTable()
         {
             ExclusionValueEditor valueEditor = new ExclusionValueEditor();
-            
+
             String oldValue = getSelectedValueExclusionsTable();
             if ( oldValue != null )
             {
-                Object oldRawValue = valueEditor.getRawValue( null, oldValue );
-                
+                Object oldRawValue = valueEditor.getRawValue( connection, oldValue );
+
                 CellEditor cellEditor = valueEditor.getCellEditor();
                 cellEditor.setValue( oldRawValue );
                 cellEditor.activate();
                 Object newRawValue = cellEditor.getValue();
-                
-                if(newRawValue != null) 
+
+                if ( newRawValue != null )
                 {
-                    String newValue = (String) valueEditor.getStringOrBinaryValue( newRawValue );
-                    
-//                    values.remove( oldValue );
-//                    values.add( newValue );
-//                    tableViewer.refresh();
+                    String newValue = ( String ) valueEditor.getStringOrBinaryValue( newRawValue );
+
+                    exclusions.remove( oldValue );
+                    exclusions.add( newValue );
+                    exclusionsTableViewer.refresh();
                 }
             }
         }
 
 
         /**
-         * TODO deleteValueExclusionsTable.
-         *
+         * Deletes the currently selected Exclusion value from list.
          */
         private void deleteValueExclusionsTable()
         {
-            // TODO Auto-generated method stub
-
+            String value = getSelectedValueExclusionsTable();
+            if ( value != null )
+            {
+                exclusions.remove( value );
+                exclusionsTableViewer.refresh();
+            }
         }
 
 
@@ -381,8 +497,6 @@ public class SubtreeSpecificationValueEditor extends AbstractDialogStringValueEd
             refinementsTableViewer = new TableViewer( refinementsTable );
             refinementsTableViewer.setContentProvider( new ArrayContentProvider() );
             refinementsTableViewer.setLabelProvider( new LabelProvider() );
-            refinementsTableViewer.setInput( new String[]
-                { "and:{ item:35.5.2.1, item:inetOrgPerson }" } );
             refinementsTableViewer.addSelectionChangedListener( new ISelectionChangedListener()
             {
                 public void selectionChanged( SelectionChangedEvent event )
@@ -515,7 +629,40 @@ public class SubtreeSpecificationValueEditor extends AbstractDialogStringValueEd
 
         private void deleteValueRefinementsTable()
         {
+
+            // TODO Auto-generated method stub
         }
 
+    }
+
+    /**
+     * The SubtreeSpecificationValueWrapper is used to pass contextual 
+     * information to the opened SubtreeSpecificationDialog.
+     *
+     * @author <a href="mailto:dev@directory.apache.org">Apache Directory Project</a>
+     * @version $Rev$, $Date$
+     */
+    private class SubtreeSpecificationValueWrapper
+    {
+        /** The connection, used in DnDialog to browse for an entry */
+        private IConnection connection;
+
+        /** The subtreeSpecification */
+        private SubtreeSpecification subtreeSpecification;
+
+
+        /**
+         * Creates a new instance of SubtreeSpecificationValueWrapper.
+         *
+         * @param connection
+         *      the connection
+         * @param dn
+         *      the DN
+         */
+        private SubtreeSpecificationValueWrapper( IConnection connection, SubtreeSpecification subtreeSpecification )
+        {
+            this.connection = connection;
+            this.subtreeSpecification = subtreeSpecification;
+        }
     }
 }
