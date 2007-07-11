739352797cb0e0da4d76d8e3b3dc8f02aef71435
Pierre-Arnaud Marcelot <pamarcelot@apache.org>
2007-07-11 16:11:41 +0000

    Added a new Export wizard for schemas as OpenLdap files.
    git-svn-id: https://svn.apache.org/repos/asf/directory/studio/trunk@555322 13f79535-47bb-0310-9956-ffa450edef68

--- studio-apacheds-schemaeditor/src/main/java/org/apache/directory/studio/apacheds/schemaeditor/view/wizards/ExportSchemasAsOpenLdapWizardPage.java
+++ studio-apacheds-schemaeditor/src/main/java/org/apache/directory/studio/apacheds/schemaeditor/view/wizards/ExportSchemasAsOpenLdapWizardPage.java
@@ -45,7 +45,6 @@ import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.DirectoryDialog;
-import org.eclipse.swt.widgets.FileDialog;
 import org.eclipse.swt.widgets.Group;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Table;
@@ -55,7 +54,7 @@ import org.eclipse.ui.plugin.AbstractUIPlugin;
 
 
 /**
- * This class represents the WizardPage of the ExportSchemasAsXmlWizard.
+ * This class represents the WizardPage of the ExportSchemasAsOpenLdapWizard.
  * <p>
  * It is used to let the user enter the informations about the
  * schemas he wants to export and where to export.
@@ -63,36 +62,28 @@ import org.eclipse.ui.plugin.AbstractUIPlugin;
  * @author <a href="mailto:dev@directory.apache.org">Apache Directory Project</a>
  * @version $Rev$, $Date$
  */
-public class ExportSchemasAsXmlWizardPage extends WizardPage
+public class ExportSchemasAsOpenLdapWizardPage extends WizardPage
 {
     /** The SchemaHandler */
     private SchemaHandler schemaHandler;
-
-    public static final int EXPORT_MULTIPLE_FILES = 0;
-    public static final int EXPORT_SINGLE_FILE = 1;
 
     // UI Fields
     private CheckboxTableViewer schemasTableViewer;
     private Button schemasTableSelectAllButton;
     private Button schemasTableDeselectAllButton;
-    private Button exportMultipleFilesRadio;
-    private Label exportMultipleFilesLabel;
-    private Text exportMultipleFilesText;
-    private Button exportMultipleFilesButton;
-    private Button exportSingleFileRadio;
-    private Label exportSingleFileLabel;
-    private Text exportSingleFileText;
-    private Button exportSingleFileButton;
+    private Label exportDirectoryLabel;
+    private Text exportDirectoryText;
+    private Button exportDirectoryButton;
 
 
     /**
      * Creates a new instance of ExportSchemasAsXmlWizardPage.
      */
-    protected ExportSchemasAsXmlWizardPage()
+    protected ExportSchemasAsOpenLdapWizardPage()
     {
-        super( "ExportSchemasAsXmlWizardPage" );
-        setTitle( "Export schemas as XML file(s)" );
-        setDescription( "Please select the schemas to export as XML file(s)." );
+        super( "ExportSchemasAsOpenLdapWizardPage" );
+        setTitle( "Export schemas as OpenLdap files" );
+        setDescription( "Please select the schemas to export as OpenLdap files." );
         setImageDescriptor( AbstractUIPlugin.imageDescriptorFromPlugin( Activator.PLUGIN_ID,
             PluginConstants.IMG_SCHEMAS_EXPORT_WIZARD ) );
         schemaHandler = Activator.getDefault().getSchemaHandler();
@@ -189,80 +180,27 @@ public class ExportSchemasAsXmlWizardPage extends WizardPage
         // Export Destination Group
         Group exportDestinationGroup = new Group( composite, SWT.NULL );
         exportDestinationGroup.setText( "Export Destination" );
-        exportDestinationGroup.setLayout( new GridLayout( 4, false ) );
+        exportDestinationGroup.setLayout( new GridLayout( 3, false ) );
         exportDestinationGroup.setLayoutData( new GridData( SWT.FILL, SWT.NONE, true, false ) );
 
-        // Export Multiple Files
-        exportMultipleFilesRadio = new Button( exportDestinationGroup, SWT.RADIO );
-        exportMultipleFilesRadio.setText( "Export each schema as a separate file." );
-        exportMultipleFilesRadio.setLayoutData( new GridData( SWT.FILL, SWT.NONE, true, false, 4, 1 ) );
-        exportMultipleFilesRadio.addSelectionListener( new SelectionAdapter()
-        {
-            public void widgetSelected( SelectionEvent e )
-            {
-                exportMultipleFilesSelected();
-                dialogChanged();
-            }
-        } );
-        Label exportMultipleFilesFiller = new Label( exportDestinationGroup, SWT.NONE );
-        exportMultipleFilesFiller.setText( "    " );
-        exportMultipleFilesLabel = new Label( exportDestinationGroup, SWT.NONE );
-        exportMultipleFilesLabel.setText( "Directory:" );
-        exportMultipleFilesText = new Text( exportDestinationGroup, SWT.BORDER );
-        exportMultipleFilesText.setLayoutData( new GridData( SWT.FILL, SWT.NONE, true, false ) );
-        exportMultipleFilesText.addModifyListener( new ModifyListener()
+        exportDirectoryLabel = new Label( exportDestinationGroup, SWT.NONE );
+        exportDirectoryLabel.setText( "Directory:" );
+        exportDirectoryText = new Text( exportDestinationGroup, SWT.BORDER );
+        exportDirectoryText.setLayoutData( new GridData( SWT.FILL, SWT.NONE, true, false ) );
+        exportDirectoryText.addModifyListener( new ModifyListener()
         {
             public void modifyText( ModifyEvent e )
             {
                 dialogChanged();
             }
         } );
-        exportMultipleFilesButton = new Button( exportDestinationGroup, SWT.PUSH );
-        exportMultipleFilesButton.setText( "Browse..." );
-        exportMultipleFilesButton.addSelectionListener( new SelectionAdapter()
+        exportDirectoryButton = new Button( exportDestinationGroup, SWT.PUSH );
+        exportDirectoryButton.setText( "Browse..." );
+        exportDirectoryButton.addSelectionListener( new SelectionAdapter()
         {
             public void widgetSelected( SelectionEvent e )
             {
                 chooseExportDirectory();
-                dialogChanged();
-            }
-        } );
-
-        // Export Single File
-        exportSingleFileRadio = new Button( exportDestinationGroup, SWT.RADIO );
-        exportSingleFileRadio.setText( "Export the schemas as a single file." );
-        exportSingleFileRadio.setLayoutData( new GridData( SWT.FILL, SWT.NONE, true, false, 4, 1 ) );
-        exportSingleFileRadio.addSelectionListener( new SelectionAdapter()
-        {
-            public void widgetSelected( SelectionEvent e )
-            {
-                exportSingleFileSelected();
-                dialogChanged();
-            }
-        } );
-        Label exportSingleFileFiller = new Label( exportDestinationGroup, SWT.NONE );
-        exportSingleFileFiller.setText( "    " );
-        exportSingleFileLabel = new Label( exportDestinationGroup, SWT.NONE );
-        exportSingleFileLabel.setText( "Export File:" );
-        exportSingleFileText = new Text( exportDestinationGroup, SWT.BORDER );
-        exportSingleFileText.setLayoutData( new GridData( SWT.FILL, SWT.NONE, true, false ) );
-        exportSingleFileText.addModifyListener( new ModifyListener()
-        {
-            public void modifyText( ModifyEvent e )
-            {
-                dialogChanged();
-            }
-        } );
-        exportSingleFileButton = new Button( exportDestinationGroup, SWT.PUSH );
-        exportSingleFileButton.setText( "Browse..." );
-        exportSingleFileButton.addSelectionListener( new SelectionAdapter()
-        {
-            /* (non-Javadoc)
-             * @see org.eclipse.swt.events.SelectionAdapter#widgetSelected(org.eclipse.swt.events.SelectionEvent)
-             */
-            public void widgetSelected( SelectionEvent e )
-            {
-                chooseExportFile();
                 dialogChanged();
             }
         } );
@@ -281,45 +219,8 @@ public class ExportSchemasAsXmlWizardPage extends WizardPage
         // Filling the Schemas table
         schemasTableViewer.setInput( schemaHandler.getSchemas() );
 
-        // Selecting the Multiple Files choice
-        exportMultipleFilesSelected();
-
         displayErrorMessage( null );
         setPageComplete( false );
-    }
-
-
-    /**
-     * This method is called when the exportMultipleFiles radio button is selected.
-     */
-    private void exportMultipleFilesSelected()
-    {
-        exportMultipleFilesRadio.setSelection( true );
-        exportMultipleFilesLabel.setEnabled( true );
-        exportMultipleFilesText.setEnabled( true );
-        exportMultipleFilesButton.setEnabled( true );
-
-        exportSingleFileRadio.setSelection( false );
-        exportSingleFileLabel.setEnabled( false );
-        exportSingleFileText.setEnabled( false );
-        exportSingleFileButton.setEnabled( false );
-    }
-
-
-    /**
-     * This method is called when the exportSingleFile radio button is selected.
-     */
-    private void exportSingleFileSelected()
-    {
-        exportMultipleFilesRadio.setSelection( false );
-        exportMultipleFilesLabel.setEnabled( false );
-        exportMultipleFilesText.setEnabled( false );
-        exportMultipleFilesButton.setEnabled( false );
-
-        exportSingleFileRadio.setSelection( true );
-        exportSingleFileLabel.setEnabled( true );
-        exportSingleFileText.setEnabled( true );
-        exportSingleFileButton.setEnabled( true );
     }
 
 
@@ -335,28 +236,7 @@ public class ExportSchemasAsXmlWizardPage extends WizardPage
         String selectedDirectory = dialog.open();
         if ( selectedDirectory != null )
         {
-            exportMultipleFilesText.setText( selectedDirectory );
-        }
-    }
-
-
-    /**
-     * This method is called when the exportSingleFile 'browse' button is selected.
-     */
-    private void chooseExportFile()
-    {
-        FileDialog dialog = new FileDialog( PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell(), SWT.SAVE );
-        dialog.setText( "Select File" );
-        dialog.setFilterExtensions( new String[]
-            { "*.xml", "*" } );
-        dialog.setFilterNames( new String[]
-            { "XML Files", "All Files" } );
-        dialog.setFilterPath( exportSingleFileText.getText() );
-
-        String selectedFile = dialog.open();
-        if ( selectedFile != null )
-        {
-            exportSingleFileText.setText( selectedFile );
+            exportDirectoryText.setText( selectedDirectory );
         }
     }
 
@@ -373,51 +253,30 @@ public class ExportSchemasAsXmlWizardPage extends WizardPage
             return;
         }
 
-        // Export option
-        if ( exportMultipleFilesRadio.getSelection() )
+        // Export Directory
+        String directory = exportDirectoryText.getText();
+        if ( ( directory == null ) || ( directory.equals( "" ) ) )
         {
-            String directory = exportMultipleFilesText.getText();
-            if ( ( directory == null ) || ( directory.equals( "" ) ) )
-            {
-                displayErrorMessage( "A directory must be selected." );
-                return;
-            }
-            else
-            {
-                File directoryFile = new File( directory );
-                if ( !directoryFile.exists() )
-                {
-                    displayErrorMessage( "The selected directory does not exist." );
-                    return;
-                }
-                else if ( !directoryFile.isDirectory() )
-                {
-                    displayErrorMessage( "The selected directory is not a directory." );
-                    return;
-                }
-                else if ( !directoryFile.canWrite() )
-                {
-                    displayErrorMessage( "The selected directory is not writable." );
-                    return;
-                }
-            }
+            displayErrorMessage( "A directory must be selected." );
+            return;
         }
-        else if ( exportSingleFileRadio.getSelection() )
+        else
         {
-            String exportFile = exportSingleFileText.getText();
-            if ( ( exportFile == null ) || ( exportFile.equals( "" ) ) )
+            File directoryFile = new File( directory );
+            if ( !directoryFile.exists() )
             {
-                displayErrorMessage( "A file must be selected." );
+                displayErrorMessage( "The selected directory does not exist." );
                 return;
             }
-            else
+            else if ( !directoryFile.isDirectory() )
             {
-                File file = new File( exportFile );
-                if ( !file.getParentFile().canWrite() )
-                {
-                    displayErrorMessage( "The selected file is not writable." );
-                    return;
-                }
+                displayErrorMessage( "The selected directory is not a directory." );
+                return;
+            }
+            else if ( !directoryFile.canWrite() )
+            {
+                displayErrorMessage( "The selected directory is not writable." );
+                return;
             }
         }
 
@@ -460,30 +319,6 @@ public class ExportSchemasAsXmlWizardPage extends WizardPage
 
 
     /**
-     * Gets the type of export.
-     * <p>
-     * Values can either EXPORT_MULTIPLE_FILES or EXPORT_SINGLE_FILE.
-     * 
-     * @return
-     *      the type of export
-     */
-    public int getExportType()
-    {
-        if ( exportMultipleFilesRadio.getSelection() )
-        {
-            return EXPORT_MULTIPLE_FILES;
-        }
-        else if ( exportSingleFileRadio.getSelection() )
-        {
-            return EXPORT_SINGLE_FILE;
-        }
-
-        // Default 
-        return EXPORT_MULTIPLE_FILES;
-    }
-
-
-    /**
      * Gets the export directory.
      *
      * @return
@@ -491,18 +326,6 @@ public class ExportSchemasAsXmlWizardPage extends WizardPage
      */
     public String getExportDirectory()
     {
-        return exportMultipleFilesText.getText();
-    }
-
-
-    /**
-     * Gets the export file.
-     *
-     * @return
-     *      the export file
-     */
-    public String getExportFile()
-    {
-        return exportSingleFileText.getText();
+        return exportDirectoryText.getText();
     }
 }
