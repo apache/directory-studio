ce07442c5d49d4436aff4a074029553b8a959246
Pierre-Arnaud Marcelot <pamarcelot@apache.org>
2008-04-16 21:21:39 +0000

    Added a SHA1 signing to the sign utility.
    git-svn-id: https://svn.apache.org/repos/asf/directory/studio/trunk@648857 13f79535-47bb-0310-9956-ffa450edef68

--- tools/sign.sh
+++ tools/sign.sh
@@ -18,6 +18,8 @@ for FILE in $(find . -not '(' -name "*.md5″ -or -name "*.sha1″ -or -name "*.
     echo -n "Signing: $FILE ... "
 
     openssl md5 < "$FILE" | cut "-d " -f1 > "$FILE.md5"
+    
+    gpg --print-md SHA1 "$FILE" > "$FILE".sha
 
     echo "$PASSWORD" | gpg --default-key "$DEFAULT_KEY" --detach-sign --armor --no-tty --yes --passphrase-fd 0 "$FILE" && echo done.
 done
