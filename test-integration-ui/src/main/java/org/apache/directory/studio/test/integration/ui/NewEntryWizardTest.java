cdfb431b7afa8f172ee20ef00f6a424289ab3866
Stefan Seelmann <seelmann@apache.org>
2008-08-02 17:33:55 +0000

    Fix for DIRSTUDIO-360 (problem with slash '/' in DN)
    git-svn-id: https://svn.apache.org/repos/asf/directory/studio/branches/1.2.x@682023 13f79535-47bb-0310-9956-ffa450edef68

--- test-integration-ui/src/main/java/org/apache/directory/studio/test/integration/ui/NewEntryWizardTest.java
+++ test-integration-ui/src/main/java/org/apache/directory/studio/test/integration/ui/NewEntryWizardTest.java
@@ -21,6 +21,10 @@
 package org.apache.directory.studio.test.integration.ui;
 
 
+import javax.naming.directory.Attribute;
+import javax.naming.directory.Attributes;
+import javax.naming.directory.DirContext;
+
 import net.sf.swtbot.eclipse.finder.SWTEclipseBot;
 import net.sf.swtbot.wait.DefaultCondition;
 import net.sf.swtbot.widgets.SWTBotCombo;
@@ -30,6 +34,8 @@ import net.sf.swtbot.widgets.SWTBotTree;
 import net.sf.swtbot.widgets.SWTBotTreeItem;
 
 import org.apache.directory.server.unit.AbstractServerTest;
+import org.apache.directory.shared.ldap.message.AttributeImpl;
+import org.apache.directory.shared.ldap.message.ModificationItemImpl;
 
 
 /**
@@ -46,6 +52,23 @@ public class NewEntryWizardTest extends AbstractServerTest
     protected void setUp() throws Exception
     {
         super.setUp();
+
+        // check if krb5kdc is disabled
+        Attributes krb5kdcAttrs = schemaRoot.getAttributes( "cn=Krb5kdc" );
+        boolean isKrb5KdcDisabled = false;
+        if ( krb5kdcAttrs.get( "m-disabled" ) != null )
+        {
+            isKrb5KdcDisabled = ( ( String ) krb5kdcAttrs.get( "m-disabled" ).get() ).equalsIgnoreCase( "TRUE" );
+        }
+        // if krb5kdc is disabled then enable it
+        if ( isKrb5KdcDisabled )
+        {
+            Attribute disabled = new AttributeImpl( "m-disabled" );
+            ModificationItemImpl[] mods = new ModificationItemImpl[]
+                { new ModificationItemImpl( DirContext.REMOVE_ATTRIBUTE, disabled ) };
+            schemaRoot.modifyAttributes( "cn=Krb5kdc", mods );
+        }
+
         bot = new SWTEclipseBot();
         SWTBotUtils.openLdapPerspective( bot );
         SWTBotUtils.createTestConnection( bot, "NewEntryWizardTest", ldapServer.getIpPort() );
@@ -201,7 +224,7 @@ public class NewEntryWizardTest extends AbstractServerTest
         } );
     }
 
-    
+
     /**
      * Test for DIRSTUDIO-350.
      * 
@@ -269,10 +292,10 @@ public class NewEntryWizardTest extends AbstractServerTest
                 return "Could not find widget";
             }
         } );
-        
+
         // Now create a second entry under the previously created entry 
         // to ensure that the selected parent is also upper case.
-        
+
         // open "New Entry" wizard
         contextMenu = browserTree.contextMenu( "New Entry..." );
         contextMenu.click();
@@ -285,17 +308,17 @@ public class NewEntryWizardTest extends AbstractServerTest
         bot.table( 0 ).select( "organization" );
         bot.button( "Add" ).click();
         bot.button( "Next >" ).click();
-        
+
         // specify DN
         typeCombo = bot.comboBoxWithLabel( "RDN:" );
         typeCombo.setText( "O" );
         valueText = bot.text( "" );
         valueText.setText( "testCreateOrganizationEntry2" );
-        
+
         // check preview text 
         SWTBotText previewText = bot.text( "O=testCreateOrganizationEntry2,O=testCreateOrganizationEntry,ou=system" );
         assertEquals( "O=testCreateOrganizationEntry2,O=testCreateOrganizationEntry,ou=system", previewText.getText() );
-        
+
         bot.button( "Next >" ).click();
 
         // wait for check that entry doesn't exist yet
@@ -328,6 +351,95 @@ public class NewEntryWizardTest extends AbstractServerTest
             public String getFailureMessage()
             {
                 return "Could not find widget";
+            }
+        } );
+    }
+
+
+    /**
+     * Test for DIRSTUDIO-360.
+     * 
+     * Create entries with a slash '/' in the RDN value.
+     * 
+     * @throws Exception the exception
+     */
+    public void testCreateEntryWithSlash() throws Exception
+    {
+        final SWTBotTree browserTree = SWTBotUtils.getLdapBrowserTree( bot );
+        SWTBotTreeItem systemNode = SWTBotUtils.selectNode( bot, browserTree, "DIT", "Root DSE", "ou=system" );
+        systemNode.expand();
+        systemNode.expand();
+
+        // open "New Entry" wizard
+        SWTBotMenu contextMenu = browserTree.contextMenu( "New Entry..." );
+        contextMenu.click();
+
+        // select entry creation method
+        bot.radio( "Create entry from scratch" ).click();
+        bot.button( "Next >" ).click();
+
+        // select object classes
+        bot.table( 0 ).select( "krb5Principal" );
+        bot.button( "Add" ).click();
+        bot.table( 0 ).select( "person" );
+        bot.button( "Add" ).click();
+        bot.button( "Next >" ).click();
+
+        // specify DN
+        SWTBotCombo typeCombo = bot.comboBoxWithLabel( "RDN:" );
+        typeCombo.setText( "krb5PrincipalName" );
+        SWTBotText valueText = bot.text( "" );
+        valueText.setText( "kadmin/changepw@DOMAIN" );
+        bot.button( "Next >" ).click();
+
+        // wait for check that entry doesn't exist yet
+        bot.waitUntil( new DefaultCondition()
+        {
+            public boolean test() throws Exception
+            {
+                return bot.tree( 0 ) != null;
+            }
+
+
+            public String getFailureMessage()
+            {
+                return "Could not find entry editor";
+            }
+        } );
+
+        // click to finish editing of sn
+        SWTBotTree tree = bot.tree( 0 );
+        SWTBotTreeItem krbNode = SWTBotUtils.selectNode( bot, tree, "krb5PrincipalName" );
+        krbNode.click();
+
+        // enter sn value
+        tree.select( "sn" );
+        bot.text( "" ).setText( "test" );
+        // click to finish editing of sn
+        krbNode.click();
+
+        // enter cn value
+        tree.select( "cn" );
+        bot.text( "" ).setText( "test" );
+        // click to finish editing of cn
+        krbNode.click();
+
+        // click finish to create the entry
+        bot.button( "Finish" ).click();
+
+        // wait till entry is created and selected in the tree
+        bot.waitUntil( new DefaultCondition()
+        {
+            public boolean test() throws Exception
+            {
+                return browserTree.selection().get( 0 ).get( 0 )
+                    .startsWith( "krb5PrincipalName=kadmin/changepw@DOMAIN" );
+            }
+
+
+            public String getFailureMessage()
+            {
+                return "Could not find entry 'krb5Principal=kadmin/changepw@DOMAIN'";
             }
         } );
     }
