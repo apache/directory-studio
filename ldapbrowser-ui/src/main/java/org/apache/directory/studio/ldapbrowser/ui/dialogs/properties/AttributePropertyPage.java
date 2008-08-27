71d2f92ca510e69d82289cd682b63a06f4ffea6a
Pierre-Arnaud Marcelot <pamarcelot@apache.org>
2008-08-27 14:11:47 +0000

    Part of a fix for DIRSTUDIO-380 (Attribute and Entry property pages makes the dialog very tall)
    git-svn-id: https://svn.apache.org/repos/asf/directory/studio/trunk@689487 13f79535-47bb-0310-9956-ffa450edef68

--- ldapbrowser-ui/src/main/java/org/apache/directory/studio/ldapbrowser/ui/dialogs/properties/AttributePropertyPage.java
+++ ldapbrowser-ui/src/main/java/org/apache/directory/studio/ldapbrowser/ui/dialogs/properties/AttributePropertyPage.java
@@ -29,8 +29,10 @@ import org.apache.directory.studio.ldapbrowser.core.model.schema.Schema;
 import org.apache.directory.studio.ldapbrowser.core.model.schema.SchemaUtils;
 import org.apache.directory.studio.ldapbrowser.core.utils.Utils;
 import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
@@ -95,30 +97,54 @@ public class AttributePropertyPage extends PropertyPage implements IWorkbenchPro
 
         BaseWidgetUtils.createLabel( mainGroup, "Description:", 1 );
         attributeNameText = BaseWidgetUtils.createLabeledText( mainGroup, "", 1 );
+        GridData attributeNameTextGridData = new GridData( SWT.FILL, SWT.NONE, true, false );
+        attributeNameTextGridData.widthHint = 300;
+        attributeNameText.setLayoutData( attributeNameTextGridData );
 
         BaseWidgetUtils.createLabel( mainGroup, "Type:", 1 );
         attributeTypeText = BaseWidgetUtils.createLabeledText( mainGroup, "", 1 );
+        GridData attributeTypeTextGridData = new GridData( SWT.FILL, SWT.NONE, true, false );
+        attributeTypeTextGridData.widthHint = 300;
+        attributeTypeText.setLayoutData( attributeTypeTextGridData );
 
         BaseWidgetUtils.createLabel( mainGroup, "Number of Values:", 1 );
         attributeValuesText = BaseWidgetUtils.createLabeledText( mainGroup, "", 1 );
+        GridData attributeValuesTextGridData = new GridData( SWT.FILL, SWT.NONE, true, false );
+        attributeValuesTextGridData.widthHint = 300;
+        attributeValuesText.setLayoutData( attributeValuesTextGridData );
 
         BaseWidgetUtils.createLabel( mainGroup, "Attribute Size:", 1 );
         attributeSizeText = BaseWidgetUtils.createLabeledText( mainGroup, "", 1 );
+        GridData attributeSizeTextGridData = new GridData( SWT.FILL, SWT.NONE, true, false );
+        attributeSizeTextGridData.widthHint = 300;
+        attributeSizeText.setLayoutData( attributeSizeTextGridData );
 
         Group atdGroup = BaseWidgetUtils.createGroup( composite, "Attribute Type", 1 );
         Composite atdComposite = BaseWidgetUtils.createColumnContainer( atdGroup, 2, 1 );
 
         BaseWidgetUtils.createLabel( atdComposite, "Numeric OID:", 1 );
         atdOidText = BaseWidgetUtils.createLabeledText( atdComposite, "", 1 );
+        GridData atdOidTextGridData = new GridData( SWT.FILL, SWT.NONE, true, false );
+        atdOidTextGridData.widthHint = 300;
+        atdOidText.setLayoutData( atdOidTextGridData );
 
         BaseWidgetUtils.createLabel( atdComposite, "Alternative Names:", 1 );
         atdNamesText = BaseWidgetUtils.createLabeledText( atdComposite, "", 1 );
+        GridData atdNamesTextGridData = new GridData( SWT.FILL, SWT.NONE, true, false );
+        atdNamesTextGridData.widthHint = 300;
+        atdNamesText.setLayoutData( atdNamesTextGridData );
 
         BaseWidgetUtils.createLabel( atdComposite, "Description:", 1 );
         atdDescText = BaseWidgetUtils.createWrappedLabeledText( atdComposite, "", 1 );
+        GridData atdDescTextGridData = new GridData( SWT.FILL, SWT.NONE, true, false );
+        atdDescTextGridData.widthHint = 300;
+        atdDescText.setLayoutData( atdDescTextGridData );
 
         BaseWidgetUtils.createLabel( atdComposite, "Usage:", 1 );
         atdUsageText = BaseWidgetUtils.createLabeledText( atdComposite, "", 1 );
+        GridData atdUsageTextGridData = new GridData( SWT.FILL, SWT.NONE, true, false );
+        atdUsageTextGridData.widthHint = 300;
+        atdUsageText.setLayoutData( atdUsageTextGridData );
 
         Group flagsGroup = BaseWidgetUtils.createGroup( composite, "Flags", 1 );
         Composite flagsComposite = BaseWidgetUtils.createColumnContainer( flagsGroup, 4, 1 );
@@ -164,24 +190,42 @@ public class AttributePropertyPage extends PropertyPage implements IWorkbenchPro
 
         BaseWidgetUtils.createLabel( syntaxComposite, "Syntax OID:", 1 );
         syntaxOidText = BaseWidgetUtils.createLabeledText( syntaxComposite, "", 1 );
+        GridData syntaxOidTextGridData = new GridData( SWT.FILL, SWT.NONE, true, false );
+        syntaxOidTextGridData.widthHint = 300;
+        syntaxOidText.setLayoutData( syntaxOidTextGridData );
 
         BaseWidgetUtils.createLabel( syntaxComposite, "Syntax Description:", 1 );
         syntaxDescText = BaseWidgetUtils.createLabeledText( syntaxComposite, "", 1 );
+        GridData syntaxDescTextGridData = new GridData( SWT.FILL, SWT.NONE, true, false );
+        syntaxDescTextGridData.widthHint = 300;
+        syntaxDescText.setLayoutData( syntaxDescTextGridData );
 
         BaseWidgetUtils.createLabel( syntaxComposite, "Syntax Length:", 1 );
         syntaxLengthText = BaseWidgetUtils.createLabeledText( syntaxComposite, "", 1 );
+        GridData syntaxLengthTextGridData = new GridData( SWT.FILL, SWT.NONE, true, false );
+        syntaxLengthTextGridData.widthHint = 300;
+        syntaxLengthText.setLayoutData( syntaxLengthTextGridData );
 
         Group matchingGroup = BaseWidgetUtils.createGroup( composite, "Matching Rules", 1 );
         Composite matchingComposite = BaseWidgetUtils.createColumnContainer( matchingGroup, 2, 1 );
 
         BaseWidgetUtils.createLabel( matchingComposite, "Equality Match:", 1 );
         equalityMatchingRuleText = BaseWidgetUtils.createLabeledText( matchingComposite, "", 1 );
+        GridData equalityMatchingRuleTextGridData = new GridData( SWT.FILL, SWT.NONE, true, false );
+        equalityMatchingRuleTextGridData.widthHint = 300;
+        equalityMatchingRuleText.setLayoutData( equalityMatchingRuleTextGridData );
 
         BaseWidgetUtils.createLabel( matchingComposite, "Substring Match:", 1 );
         substringMatchingRuleText = BaseWidgetUtils.createLabeledText( matchingComposite, "", 1 );
+        GridData substringMatchingRuleTextGridData = new GridData( SWT.FILL, SWT.NONE, true, false );
+        substringMatchingRuleTextGridData.widthHint = 300;
+        substringMatchingRuleText.setLayoutData( substringMatchingRuleTextGridData );
 
         BaseWidgetUtils.createLabel( matchingComposite, "Ordering Match:", 1 );
         orderingMatchingRuleText = BaseWidgetUtils.createLabeledText( matchingComposite, "", 1 );
+        GridData orderingMatchingRuleTextGridData = new GridData( SWT.FILL, SWT.NONE, true, false );
+        orderingMatchingRuleTextGridData.widthHint = 300;
+        orderingMatchingRuleText.setLayoutData( orderingMatchingRuleTextGridData );
 
         IAttribute attribute = getAttribute( getElement() );
         if ( attribute != null )
