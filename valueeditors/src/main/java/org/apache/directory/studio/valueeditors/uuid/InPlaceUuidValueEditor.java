01e12e2875e09a46b06569af5c686269e4086667
Kiran Ayyagari <kayyagari@apache.org>
2009-05-25 16:30:25 +0000

    an editor to show entryUUID attribute value
    git-svn-id: https://svn.apache.org/repos/asf/directory/studio/trunk@778450 13f79535-47bb-0310-9956-ffa450edef68

---
+++ valueeditors/src/main/java/org/apache/directory/studio/valueeditors/uuid/InPlaceUuidValueEditor.java
@@ -0,0 +1,64 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *  
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License. 
+ *  
+ */
+
+package org.apache.directory.studio.valueeditors.uuid;
+
+import java.util.UUID;
+
+import org.apache.directory.studio.ldapbrowser.core.model.IValue;
+import org.apache.directory.studio.valueeditors.AbstractInPlaceStringValueEditor;
+
+
+
+
+/**
+ * Implementation of IValueEditor for attribute 'entryUUID' with syntax 1.3.6.1.4.1.18060.0.4.1.2.29 
+ * . 
+ * 
+ * Currently only the getDisplayXXX() methods are implemented.
+ * For modification the raw string must be edited.
+ *
+ * @author <a href="mailto:dev@directory.apache.org">Apache Directory Project</a>
+ * @version $Rev$, $Date$
+ */
+public class InPlaceUuidValueEditor extends AbstractInPlaceStringValueEditor
+{
+
+    /**
+     * {@inheritDoc}
+     */
+    public String getDisplayValue( IValue value )
+    {
+        byte[] displayValue = value.getBinaryValue();
+
+        if ( !showRawValues() )
+        {
+        	if( displayValue == null )
+        	{
+        		return "NULL";
+        	}
+        	
+        	return UUID.nameUUIDFromBytes( displayValue ).toString(); //$NON-NLS-1$ //$NON-NLS-2$
+        }
+
+        return "Binay entryUUID value";
+    }
+
+}
