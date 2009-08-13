809d66ab508067eadbd71364775d6ba743612dbb
Stefan Seelmann <seelmann@apache.org>
2009-08-13 15:59:09 +0000

    Formatting
    git-svn-id: https://svn.apache.org/repos/asf/directory/studio/trunk@803937 13f79535-47bb-0310-9956-ffa450edef68

--- ldapbrowser-core/src/main/java/org/apache/directory/studio/ldapbrowser/core/BrowserCorePreferencesInitializer.java
+++ ldapbrowser-core/src/main/java/org/apache/directory/studio/ldapbrowser/core/BrowserCorePreferencesInitializer.java
@@ -44,20 +44,20 @@ public class BrowserCorePreferencesInitializer extends AbstractPreferenceInitial
 
         store.setDefault( BrowserCoreConstants.PREFERENCE_CHECK_FOR_CHILDREN, true );
 
-        store.setDefault( BrowserCoreConstants.PREFERENCE_FORMAT_CSV_ATTRIBUTEDELIMITER, "," );
-        store.setDefault( BrowserCoreConstants.PREFERENCE_FORMAT_CSV_VALUEDELIMITER, "|" );
-        store.setDefault( BrowserCoreConstants.PREFERENCE_FORMAT_CSV_QUOTECHARACTER, "\"" );
+        store.setDefault( BrowserCoreConstants.PREFERENCE_FORMAT_CSV_ATTRIBUTEDELIMITER, "," ); //$NON-NLS-1$
+        store.setDefault( BrowserCoreConstants.PREFERENCE_FORMAT_CSV_VALUEDELIMITER, "|" ); //$NON-NLS-1$
+        store.setDefault( BrowserCoreConstants.PREFERENCE_FORMAT_CSV_QUOTECHARACTER, "\"" ); //$NON-NLS-1$
         store
             .setDefault( BrowserCoreConstants.PREFERENCE_FORMAT_CSV_LINESEPARATOR, BrowserCoreConstants.LINE_SEPARATOR );
         store.setDefault( BrowserCoreConstants.PREFERENCE_FORMAT_CSV_BINARYENCODING,
             BrowserCoreConstants.BINARYENCODING_IGNORE );
         store.setDefault( BrowserCoreConstants.PREFERENCE_FORMAT_CSV_ENCODING, BrowserCoreConstants.DEFAULT_ENCODING );
 
-        store.setDefault( BrowserCoreConstants.PREFERENCE_FORMAT_XLS_VALUEDELIMITER, "|" );
+        store.setDefault( BrowserCoreConstants.PREFERENCE_FORMAT_XLS_VALUEDELIMITER, "|" ); //$NON-NLS-1$
         store.setDefault( BrowserCoreConstants.PREFERENCE_FORMAT_XLS_BINARYENCODING,
             BrowserCoreConstants.BINARYENCODING_IGNORE );
 
-        store.setDefault( BrowserCoreConstants.PREFERENCE_FORMAT_ODF_VALUEDELIMITER, "|" );
+        store.setDefault( BrowserCoreConstants.PREFERENCE_FORMAT_ODF_VALUEDELIMITER, "|" ); //$NON-NLS-1$
         store.setDefault( BrowserCoreConstants.PREFERENCE_FORMAT_ODF_BINARYENCODING,
             BrowserCoreConstants.BINARYENCODING_IGNORE );
 
@@ -67,59 +67,33 @@ public class BrowserCorePreferencesInitializer extends AbstractPreferenceInitial
 
         // default binary attributes
         BinaryAttribute[] defaultBinaryAttributes = new BinaryAttribute[]
-            { new BinaryAttribute( "0.9.2342.19200300.100.1.7" ), // photo
-                // //$NON-NLS-1$
-                new BinaryAttribute( "0.9.2342.19200300.100.1.53" ), // personalSignature
-                // //$NON-NLS-1$
-                new BinaryAttribute( "0.9.2342.19200300.100.1.55" ), // audio
-                // //$NON-NLS-1$
-                new BinaryAttribute( "0.9.2342.19200300.100.1.60" ), // jpegPhoto
-                // //$NON-NLS-1$
-                new BinaryAttribute( "1.3.6.1.4.1.42.2.27.4.1.8" ), // javaSerializedData
-                // //$NON-NLS-1$
-                new BinaryAttribute( "1.3.6.1.4.1.1466.101.120.35" ), // thumbnailPhoto
-                // //$NON-NLS-1$
-                new BinaryAttribute( "1.3.6.1.4.1.1466.101.120.36" ), // thumbnailLogo
-                // //$NON-NLS-1$
-                new BinaryAttribute( "2.5.4.35" ), // userPassword
-                // //$NON-NLS-1$
-                new BinaryAttribute( "2.5.4.36" ), // userCertificate
-                // //$NON-NLS-1$
-                new BinaryAttribute( "2.5.4.37" ), // cACertificate
-                // //$NON-NLS-1$
-                new BinaryAttribute( "2.5.4.38" ), // authorityRevocationList
-                // //$NON-NLS-1$
-                new BinaryAttribute( "2.5.4.39" ), // certificateRevocationList
-                // //$NON-NLS-1$
-                new BinaryAttribute( "2.5.4.40" ), // crossCertificatePair
-                // //$NON-NLS-1$
-                new BinaryAttribute( "2.5.4.45" ), // x500UniqueIdentifier
-            // //$NON-NLS-1$
+            { new BinaryAttribute( "0.9.2342.19200300.100.1.7" ), // photo //$NON-NLS-1$
+                new BinaryAttribute( "0.9.2342.19200300.100.1.53" ), // personalSignature //$NON-NLS-1$
+                new BinaryAttribute( "0.9.2342.19200300.100.1.55" ), // audio //$NON-NLS-1$
+                new BinaryAttribute( "0.9.2342.19200300.100.1.60" ), // jpegPhoto //$NON-NLS-1$
+                new BinaryAttribute( "1.3.6.1.4.1.42.2.27.4.1.8" ), // javaSerializedData //$NON-NLS-1$
+                new BinaryAttribute( "1.3.6.1.4.1.1466.101.120.35" ), // thumbnailPhoto //$NON-NLS-1$
+                new BinaryAttribute( "1.3.6.1.4.1.1466.101.120.36" ), // thumbnailLogo //$NON-NLS-1$
+                new BinaryAttribute( "2.5.4.35" ), // userPassword //$NON-NLS-1$
+                new BinaryAttribute( "2.5.4.36" ), // userCertificate //$NON-NLS-1$
+                new BinaryAttribute( "2.5.4.37" ), // cACertificate //$NON-NLS-1$
+                new BinaryAttribute( "2.5.4.38" ), // authorityRevocationList //$NON-NLS-1$
+                new BinaryAttribute( "2.5.4.39" ), // certificateRevocationList //$NON-NLS-1$
+                new BinaryAttribute( "2.5.4.40" ), // crossCertificatePair //$NON-NLS-1$
+                new BinaryAttribute( "2.5.4.45" ), // x500UniqueIdentifier //$NON-NLS-1$
             };
         BrowserCorePlugin.getDefault().getCorePreferences().setDefaultBinaryAttributes( defaultBinaryAttributes );
 
         // default binary syntaxes
         BinarySyntax[] defaultBinarySyntaxes = new BinarySyntax[]
-            { new BinarySyntax( "1.3.6.1.4.1.1466.115.121.1.5" ), // Binary
-                // //$NON-NLS-1$
-                new BinarySyntax( "1.3.6.1.4.1.1466.115.121.1.8" ), // Certificate
-                // //$NON-NLS-1$
-                new BinarySyntax( "1.3.6.1.4.1.1466.115.121.1.9" ), // Certificate
-                // List
-                // //$NON-NLS-1$
-                new BinarySyntax( "1.3.6.1.4.1.1466.115.121.1.10" ), // Certificate
-                // Pair
-                // //$NON-NLS-1$
-                new BinarySyntax( "1.3.6.1.4.1.1466.115.121.1.23" ), // Fax
-                // //$NON-NLS-1$
-                new BinarySyntax( "1.3.6.1.4.1.1466.115.121.1.28" ), // JPEG
-                // //$NON-NLS-1$
-                new BinarySyntax( "1.3.6.1.4.1.1466.115.121.1.40" ), // Octet
-                // String
-                // //$NON-NLS-1$
-                new BinarySyntax( "1.3.6.1.4.1.1466.115.121.1.49" ) // Supported
-            // Algorithm
-            // //$NON-NLS-1$
+            { new BinarySyntax( "1.3.6.1.4.1.1466.115.121.1.5" ), // Binary //$NON-NLS-1$
+                new BinarySyntax( "1.3.6.1.4.1.1466.115.121.1.8" ), // Certificate //$NON-NLS-1$
+                new BinarySyntax( "1.3.6.1.4.1.1466.115.121.1.9" ), // Certificate List //$NON-NLS-1$
+                new BinarySyntax( "1.3.6.1.4.1.1466.115.121.1.10" ), // Certificate Pair //$NON-NLS-1$
+                new BinarySyntax( "1.3.6.1.4.1.1466.115.121.1.23" ), // Fax //$NON-NLS-1$
+                new BinarySyntax( "1.3.6.1.4.1.1466.115.121.1.28" ), // JPEG //$NON-NLS-1$
+                new BinarySyntax( "1.3.6.1.4.1.1466.115.121.1.40" ), // Octet String //$NON-NLS-1$
+                new BinarySyntax( "1.3.6.1.4.1.1466.115.121.1.49" ) // Supported Algorithm //$NON-NLS-1$
             };
         BrowserCorePlugin.getDefault().getCorePreferences().setDefaultBinarySyntaxes( defaultBinarySyntaxes );
     }
