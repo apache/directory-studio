9fe7b85a76210e167045f23726b8cf0b0da2102a
Pierre-Arnaud Marcelot <pamarcelot@apache.org>
2009-09-07 17:12:28 +0000

    Fix for DIRSTUDIO-534 (Re-order the preference pages for better understanding).
    git-svn-id: https://svn.apache.org/repos/asf/directory/studio/trunk@812226 13f79535-47bb-0310-9956-ffa450edef68

---
+++ common-ui/src/main/java/org/apache/directory/studio/common/StudioPreferencePage.java
@@ -0,0 +1,68 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *  
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License. 
+ *  
+ */
+package org.apache.directory.studio.common;
+
+
+import org.eclipse.jface.preference.PreferencePage;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchPreferencePage;
+
+
+/**
+ * This class implements the "Apache Directory Studio" preference page.
+ *
+ * @author <a href="mailto:dev@directory.apache.org">Apache Directory Project</a>
+ * @version $Rev$, $Date$
+ */
+public class StudioPreferencePage extends PreferencePage implements IWorkbenchPreferencePage
+{
+    /**
+     * Creates a new instance of StudioPreferencePage.
+     */
+    public StudioPreferencePage()
+    {
+        super( Messages.getString( "StudioPreferencePage.Title" ) ); //$NON-NLS-1$
+        setDescription( Messages.getString( "StudioPreferencePage.Description" ) ); //$NON-NLS-1$
+
+        // Removing Default and Apply buttons
+        noDefaultAndApplyButton();
+    }
+
+
+    /**
+     * {@inheritDoc}
+     */
+    protected Control createContents( Composite parent )
+    {
+        // Nothing to do
+        return parent;
+    }
+
+
+    /**
+     * {@inheritDoc}
+     */
+    public void init( IWorkbench workbench )
+    {
+        // Nothing to do
+    }
+}
