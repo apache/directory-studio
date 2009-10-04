067faa388017d07f490ab2a6fead765a4758f0b0
Stefan Seelmann <seelmann@apache.org>
2009-10-04 11:15:16 +0000

    Use OK button as default button
    git-svn-id: https://svn.apache.org/repos/asf/directory/studio/trunk@821509 13f79535-47bb-0310-9956-ffa450edef68

--- valueeditors/src/main/java/org/apache/directory/studio/valueeditors/integer/IntegerDialog.java
+++ valueeditors/src/main/java/org/apache/directory/studio/valueeditors/integer/IntegerDialog.java
@@ -84,7 +84,7 @@ public class IntegerDialog extends Dialog
      */
     protected void createButtonsForButtonBar( Composite parent )
     {
-        createButton( parent, IDialogConstants.OK_ID, IDialogConstants.OK_LABEL, false );
+        createButton( parent, IDialogConstants.OK_ID, IDialogConstants.OK_LABEL, true );
         createButton( parent, IDialogConstants.CANCEL_ID, IDialogConstants.CANCEL_LABEL, false );
     }
 
