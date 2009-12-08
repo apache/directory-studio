ba9ac7b2e8887264c145e53df62361d0dbc5810e
Pierre-Arnaud Marcelot <pamarcelot@apache.org>
2009-12-08 14:32:39 +0000

    Updated Release Notes file.
    git-svn-id: https://svn.apache.org/repos/asf/directory/studio/trunk@888408 13f79535-47bb-0310-9956-ffa450edef68

--- test-integration-ui/src/main/java/org/apache/directory/studio/test/integration/ui/bots/StudioBot.java
+++ test-integration-ui/src/main/java/org/apache/directory/studio/test/integration/ui/bots/StudioBot.java
@@ -57,6 +57,11 @@ public class StudioBot
     {
         return new ModificationLogsViewBot();
     }
+    
+    public ApacheDSServersViewBot getApacheDSServersViewBot()
+    {
+        return new ApacheDSServersViewBot();
+    }
 
 
     public EntryEditorBot getEntryEditorBot( String title )
