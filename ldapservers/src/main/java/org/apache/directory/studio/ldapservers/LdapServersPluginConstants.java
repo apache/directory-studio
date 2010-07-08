5eb4c85c1e1acf9e988ddc97b327dfd63f7bb0ca
Pierre-Arnaud Marcelot <pamarcelot@apache.org>
2010-07-08 15:01:13 +0000

    Created a new 'LDAP Servers' plugin.
    git-svn-id: https://svn.apache.org/repos/asf/directory/studio/trunk@961793 13f79535-47bb-0310-9956-ffa450edef68

---
+++ ldapservers/src/main/java/org/apache/directory/studio/ldapservers/LdapServersPluginConstants.java
@@ -0,0 +1,34 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *  
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License. 
+ *  
+ */
+
+package org.apache.directory.studio.ldapservers;
+
+
+/**
+ * Constants used in the LDAP Servers plugin.
+ *
+ * @author <a href="mailto:dev@directory.apache.org">Apache Directory Project</a>
+ */
+public interface LdapServersPluginConstants
+{
+    /** The plug-in ID */
+    public static final String PLUGIN_ID = LdapServersPlugin.getDefault().getPluginProperties().getString( "Plugin_id" ); //$NON-NLS-1$
+
+}
