0b8c7d05a7e303e5a862ee6479cac00020bf721f
Pierre-Arnaud Marcelot <pamarcelot@apache.org>
2010-07-13 10:04:59 +0000

    Created a common place for Common UI Utility methods.
    git-svn-id: https://svn.apache.org/repos/asf/directory/studio/trunk@963655 13f79535-47bb-0310-9956-ffa450edef68

---
+++ common-ui/src/main/java/org/apache/directory/studio/common/CommonUiUtils.java
@@ -0,0 +1,48 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *  
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License. 
+ *  
+ */
+package org.apache.directory.studio.common;
+
+
+import org.eclipse.jface.dialogs.IDialogConstants;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.ui.PlatformUI;
+
+
+/**
+ * This class contains helpful methods.
+ *
+ * @author <a href="mailto:dev@directory.apache.org">Apache Directory Project</a>
+ */
+public class CommonUiUtils
+{
+    /**
+     * Reports an error.
+     *
+     * @param message
+     *      the message
+     */
+    public static void reportError( String message )
+    {
+        MessageDialog dialog = new MessageDialog( PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell(),
+            "Error!", null, message, MessageDialog.ERROR, new String[]
+                { IDialogConstants.OK_LABEL }, MessageDialog.OK );
+        dialog.open();
+    }
+}
