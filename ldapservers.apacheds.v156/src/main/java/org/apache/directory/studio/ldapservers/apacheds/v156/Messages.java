b6804d3cfa1d98f0bec782fe75ca24107e9eeecf
Pierre-Arnaud Marcelot <pamarcelot@apache.org>
2010-08-03 16:28:26 +0000

    Fixed bundled name and removed old package.
    git-svn-id: https://svn.apache.org/repos/asf/directory/studio/trunk@981955 13f79535-47bb-0310-9956-ffa450edef68

--- ldapservers.apacheds.v156/src/main/java/org/apache/directory/studio/ldapservers/apacheds/v156/Messages.java
+++ ldapservers.apacheds.v156/src/main/java/org/apache/directory/studio/ldapservers/apacheds/v156/Messages.java
@@ -26,7 +26,7 @@ import java.util.ResourceBundle;
 
 public class Messages
 {
-    private static final String BUNDLE_NAME = "org.apache.directory.studio.connection.ui.messages"; //$NON-NLS-1$
+    private static final String BUNDLE_NAME = "org.apache.directory.studio.ldapservers.apacheds.v156.messages"; //$NON-NLS-1$
 
     private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle.getBundle( BUNDLE_NAME );
 
