d10f391f3e2c3ad3b264975e928f9b32384260e7
Emmanuel Lecharny <elecharny@apache.org>
2010-09-17 20:48:12 +0000

    removed the bizarre characters
    git-svn-id: https://svn.apache.org/repos/asf/directory/studio/trunk@998311 13f79535-47bb-0310-9956-ffa450edef68

--- tools/sign.sh
+++ tools/sign.sh
@@ -9,7 +9,7 @@ read PASSWORD
 stty echo
 echo ""
 
-for FILE in $(find . -not '(' -name "*.md5″ -or -name "*.sha1″ -or -name "*.asc" ')' -and -type f) ; do
+for FILE in $(find . -not '(' -name "*.md5" -or -name "*.sha1" -or -name "*.asc" ')' -and -type f) ; do
     if [ -f "$FILE.asc" ]; then
         echo "Skipping: $FILE"
         continue
