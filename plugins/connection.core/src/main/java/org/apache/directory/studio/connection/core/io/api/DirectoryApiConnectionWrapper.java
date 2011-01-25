b2d17cae1e85f8f398f00eb214c242e8774c9df2
Alex Karasulu <akarasulu@apache.org>
2011-01-25 00:52:40 +0000

    Shared refactoring ...
      o added AddRequestDecorator and removed Encodeable interface
      o decoupled AddRequestImpl from codec code, and moved to model
      o modified LdapEncoder to handle decorators
    
    
    
    git-svn-id: https://svn.apache.org/repos/asf/directory/studio/trunk@1063083 13f79535-47bb-0310-9956-ffa450edef68

--- plugins/connection.core/src/main/java/org/apache/directory/studio/connection/core/io/api/DirectoryApiConnectionWrapper.java
+++ plugins/connection.core/src/main/java/org/apache/directory/studio/connection/core/io/api/DirectoryApiConnectionWrapper.java
@@ -47,7 +47,7 @@ import org.apache.directory.shared.ldap.model.entry.AttributeUtils;
 import org.apache.directory.shared.ldap.model.entry.Modification;
 import org.apache.directory.shared.ldap.model.entry.ModificationOperation;
 import org.apache.directory.shared.ldap.model.message.AddRequest;
-import org.apache.directory.shared.ldap.message.AddRequestImpl;
+import org.apache.directory.shared.ldap.model.message.AddRequestImpl;
 import org.apache.directory.shared.ldap.model.message.AddResponse;
 import org.apache.directory.shared.ldap.model.message.*;
 import org.apache.directory.shared.ldap.model.message.DeleteRequest;
