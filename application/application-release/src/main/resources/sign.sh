415b345f86a5a37b0ee40a20cbb503cc009ca3bb
Pierre-Arnaud Marcelot <pamarcelot@apache.org>
2011-09-05 13:13:38 +0000

    Adapted the 'sign.sh' script for automated signature based on properties in the 'application-release' sub-module.
    git-svn-id: https://svn.apache.org/repos/asf/directory/studio/trunk@1165269 13f79535-47bb-0310-9956-ffa450edef68

--- application/application-release/src/main/resources/sign.sh
+++ application/application-release/src/main/resources/sign.sh
@@ -1,21 +1,31 @@
 #!/bin/sh
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+# 
+# http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
 
-echo "PGP Key ID: "
-read DEFAULT_KEY
+DEFAULT_KEY=@gpg.keyname@
+PASSWORD=@gpg.passphrase@
 
-echo "PGP Key Password: "
-stty -echo
-read PASSWORD
-stty echo
-echo ""
-
-for FILE in $(find . -maxdepth 1 -not '(' -name "sign.sh" -or -name ".*" -or -name "*.md5" -or -name "*.sha1" -or -name "*.asc" ')' -and -type f) ; do
+for FILE in $(find . -not '(' -name "sign.sh" -or -name ".*" -or -name "*.md5" -or -name "*.sha1" -or -name "*.asc" ')' -and -type f) ; do
     if [ -f "$FILE.asc" ]; then
         echo "Skipping: $FILE"
         continue
     fi
 
-    echo -n "Signing: $FILE ... "
+    echo "Signing: $FILE ... "
 
     # MD5
     if [ ! -f "$FILE.md5" ];
