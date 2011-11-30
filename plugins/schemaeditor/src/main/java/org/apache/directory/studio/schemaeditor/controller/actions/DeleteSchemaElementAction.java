7151e0682c701df6e8a0aeb93ecfd432d7fc8ebf
Pierre-Arnaud Marcelot <pamarcelot@apache.org>
2011-11-30 14:19:17 +0000

    Switched to 'MessageDialog' class instead of 'MessageBox' class for user confirmation.
    git-svn-id: https://svn.apache.org/repos/asf/directory/studio/trunk@1208447 13f79535-47bb-0310-9956-ffa450edef68

--- plugins/schemaeditor/src/main/java/org/apache/directory/studio/schemaeditor/controller/actions/DeleteSchemaElementAction.java
+++ plugins/schemaeditor/src/main/java/org/apache/directory/studio/schemaeditor/controller/actions/DeleteSchemaElementAction.java
@@ -39,17 +39,15 @@ import org.apache.directory.studio.schemaeditor.view.wrappers.ObjectClassWrapper
 import org.apache.directory.studio.schemaeditor.view.wrappers.SchemaWrapper;
 import org.eclipse.jface.action.Action;
 import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.ISelectionChangedListener;
 import org.eclipse.jface.viewers.SelectionChangedEvent;
 import org.eclipse.jface.viewers.StructuredSelection;
 import org.eclipse.jface.viewers.TreeViewer;
 import org.eclipse.osgi.util.NLS;
-import org.eclipse.swt.SWT;
-import org.eclipse.swt.widgets.MessageBox;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.IWorkbenchWindowActionDelegate;
-import org.eclipse.ui.PlatformUI;
 
 
 /**
@@ -121,38 +119,41 @@ public class DeleteSchemaElementAction extends Action implements IWorkbenchWindo
 
         if ( !selection.isEmpty() )
         {
-            MessageBox messageBox = new MessageBox( PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell(),
-                SWT.YES | SWT.NO | SWT.ICON_QUESTION );
+            StringBuilder message = new StringBuilder();
+
             int count = selection.size();
+
             if ( count == 1 )
             {
                 Object firstElement = selection.getFirstElement();
                 if ( firstElement instanceof AttributeTypeWrapper )
                 {
-                    messageBox.setMessage( Messages.getString( "DeleteSchemaElementAction.SureToDeleteAttributeType" ) ); //$NON-NLS-1$
+                    message.append( Messages.getString( "DeleteSchemaElementAction.SureDeleteAttributeType" ) ); //$NON-NLS-1$
                 }
                 else if ( firstElement instanceof ObjectClassWrapper )
                 {
-                    messageBox.setMessage( Messages.getString( "DeleteSchemaElementAction.SureToDeleteObjectClass" ) ); //$NON-NLS-1$
+                    message.append( Messages.getString( "DeleteSchemaElementAction.SureDeleteObjectClass" ) ); //$NON-NLS-1$
                 }
                 else if ( firstElement instanceof SchemaWrapper )
                 {
-                    messageBox.setMessage( Messages.getString( "DeleteSchemaElementAction.SureToDeleteSchema" ) ); //$NON-NLS-1$
+                    message.append( Messages.getString( "DeleteSchemaElementAction.SureDeleteSchema" ) ); //$NON-NLS-1$
                 }
                 else
                 {
-                    messageBox.setMessage( Messages.getString( "DeleteSchemaElementAction.SureToDeleteItem" ) ); //$NON-NLS-1$
+                    message.append( Messages.getString( "DeleteSchemaElementAction.SureDeleteItem" ) ); //$NON-NLS-1$
                 }
 
             }
             else
             {
-                messageBox.setMessage( NLS.bind(
-                    Messages.getString( "DeleteSchemaElementAction.SureToDeleteItems" ), new Object[] { count } ) ); //$NON-NLS-1$
+                message.append( NLS.bind(
+                    Messages.getString( "DeleteSchemaElementAction.SureDeleteItems" ), new Object[] { count } ) ); //$NON-NLS-1$
             }
-            if ( messageBox.open() == SWT.YES )
-            {
 
+            // Showing the confirmation window
+            if ( MessageDialog.openConfirm( viewer.getControl().getShell(),
+                Messages.getString( "DeleteSchemaElementAction.DeleteTitle" ), message.toString() ) ) //$NON-NLS-1$
+            {
                 Map<String, Schema> schemasMap = new HashMap<String, Schema>();
                 List<SchemaObject> schemaObjectsList = new ArrayList<SchemaObject>();
 
