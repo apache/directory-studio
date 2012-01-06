84ae3ff266f1cf8a94af7cd25c3a4e107a73a078
Pierre-Arnaud Marcelot <pamarcelot@apache.org>
2012-01-06 10:23:13 +0000

    Marked strings as non-externalized.
    git-svn-id: https://svn.apache.org/repos/asf/directory/studio/trunk@1228059 13f79535-47bb-0310-9956-ffa450edef68

--- plugins/aciitemeditor/src/main/java/org/apache/directory/studio/aciitemeditor/ACIITemConstants.java
+++ plugins/aciitemeditor/src/main/java/org/apache/directory/studio/aciitemeditor/ACIITemConstants.java
@@ -28,7 +28,7 @@ package org.apache.directory.studio.aciitemeditor;
 public interface ACIITemConstants
 {
     /** The plug-in ID */
-    public static final String PLUGIN_ID = Activator.getDefault().getPluginProperties().getString( "Plugin_id" );
+    public static final String PLUGIN_ID = Activator.getDefault().getPluginProperties().getString( "Plugin_id" ); //$NON-NLS-1$
 
     /** The ID for ACI Item Template */
     public static final String ACI_ITEM_TEMPLATE_ID = Activator.getDefault().getPluginProperties().getString(
