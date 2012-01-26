f7e720fb8a13533cd6affb7b034770d3c256ac85
Pierre-Arnaud Marcelot <pamarcelot@apache.org>
2012-01-26 12:52:02 +0000

    Fix for DIRSTUDIO-733 (Error while reading RootDSE on Sun One directory server).
    git-svn-id: https://svn.apache.org/repos/asf/directory/studio/trunk@1236162 13f79535-47bb-0310-9956-ffa450edef68

--- plugins/ldapbrowser.core/src/main/java/org/apache/directory/studio/ldapbrowser/core/model/impl/AbstractEntry.java
+++ plugins/ldapbrowser.core/src/main/java/org/apache/directory/studio/ldapbrowser/core/model/impl/AbstractEntry.java
@@ -223,7 +223,8 @@ public abstract class AbstractEntry implements IEntry, ICompareableEntry
             String oidString = attributeToDelete.getAttributeDescription().toOidString(
                 getBrowserConnection().getSchema() );
             AttributeInfo ai = getBrowserConnectionImpl().getAttributeInfo( this );
-            if ( ai != null && ai.attributeMap != null && ai.attributeMap.containsKey( Strings.toLowerCase( oidString ) ) )
+            if ( ai != null && ai.attributeMap != null
+                && ai.attributeMap.containsKey( Strings.toLowerCase( oidString ) ) )
             {
                 attributeToDelete = ( IAttribute ) ai.attributeMap.get( Strings.toLowerCase( oidString ) );
                 ai.attributeMap.remove( Strings.toLowerCase( oidString ) );
@@ -556,7 +557,8 @@ public abstract class AbstractEntry implements IEntry, ICompareableEntry
     {
         AttributeDescription ad = new AttributeDescription( attributeDescription );
         String oidString = ad.toOidString( getBrowserConnection().getSchema() );
-        if ( oidString.equals( SchemaConstants.OBJECT_CLASS_AT_OID ) )
+        if ( oidString.equals( SchemaConstants.OBJECT_CLASS_AT_OID )
+            || ( SchemaConstants.OBJECT_CLASS_AT.equalsIgnoreCase( attributeDescription ) ) )
         {
             return objectClassAttribute;
         }
