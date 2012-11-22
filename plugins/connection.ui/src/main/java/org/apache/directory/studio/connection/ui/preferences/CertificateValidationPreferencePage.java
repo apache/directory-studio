d9de069d8455728b0c397764c1e5c1fd230c0df7
Pierre-Arnaud Marcelot <pamarcelot@apache.org>
2012-11-22 16:49:39 +0000

    Organized imports.
    git-svn-id: https://svn.apache.org/repos/asf/directory/studio/trunk@1412618 13f79535-47bb-0310-9956-ffa450edef68

--- plugins/connection.ui/src/main/java/org/apache/directory/studio/connection/ui/preferences/CertificateValidationPreferencePage.java
+++ plugins/connection.ui/src/main/java/org/apache/directory/studio/connection/ui/preferences/CertificateValidationPreferencePage.java
@@ -32,7 +32,6 @@ import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.layout.GridData;
-import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
