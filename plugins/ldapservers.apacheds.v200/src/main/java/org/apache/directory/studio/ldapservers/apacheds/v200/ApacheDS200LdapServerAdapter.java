14aa554b318273c203f34087088f447a9e50a6c8
Pierre-Arnaud Marcelot <pamarcelot@apache.org>
2013-01-11 17:58:04 +0000

    Externalized and translated strings.
    git-svn-id: https://svn.apache.org/repos/asf/directory/studio/trunk@1432197 13f79535-47bb-0310-9956-ffa450edef68

--- plugins/ldapservers.apacheds.v200/src/main/java/org/apache/directory/studio/ldapservers/apacheds/v200/ApacheDS200LdapServerAdapter.java
+++ plugins/ldapservers.apacheds.v200/src/main/java/org/apache/directory/studio/ldapservers/apacheds/v200/ApacheDS200LdapServerAdapter.java
@@ -95,10 +95,10 @@ public class ApacheDS200LdapServerAdapter implements LdapServerAdapter
 
         // Verifying and copying ApacheDS 2.0.0 libraries
         LdapServersUtils.verifyAndCopyLibraries( bundle, new Path( RESOURCES ).append( LIBS ),
-            getServerLibrariesFolder(), libraries, monitor, "verifying and copying ApacheDS 2.0.0 libraries" );
+            getServerLibrariesFolder(), libraries, monitor, Messages.getString("ApacheDS200LdapServerAdapter.VerifyingAndCopyingLibraries") ); //$NON-NLS-1$
 
         // Creating server folder structure
-        monitor.subTask( "creating server folder structure" );
+        monitor.subTask( Messages.getString("ApacheDS200LdapServerAdapter.CreatingServerFolderStructure") ); //$NON-NLS-1$
         File serverFolder = LdapServersManager.getServerFolder( server ).toFile();
         File confFolder = new File( serverFolder, CONF );
         confFolder.mkdir();
@@ -110,7 +110,7 @@ public class ApacheDS200LdapServerAdapter implements LdapServerAdapter
         partitionFolder.mkdir();
 
         // Copying configuration files
-        monitor.subTask( "copying configuration files" );
+        monitor.subTask( Messages.getString("ApacheDS200LdapServerAdapter.CopyingConfigurationFiles") ); //$NON-NLS-1$
         IPath resourceConfFolderPath = new Path( RESOURCES ).append( CONF );
         LdapServersUtils.copyResource( bundle, resourceConfFolderPath.append( CONFIG_LDIF ), new File( confFolder,
             CONFIG_LDIF ) );
@@ -168,7 +168,7 @@ public class ApacheDS200LdapServerAdapter implements LdapServerAdapter
 
         // Verifying and copying ApacheDS 2.0.0 libraries
         LdapServersUtils.verifyAndCopyLibraries( bundle, new Path( RESOURCES ).append( LIBS ),
-            getServerLibrariesFolder(), libraries, monitor, "verifying and copying ApacheDS 2.0.0 libraries" );
+            getServerLibrariesFolder(), libraries, monitor, Messages.getString("ApacheDS200LdapServerAdapter.VerifyingAndCopyingLibraries") ); //$NON-NLS-1$
 
         // Starting the console printer thread
         LdapServersUtils.startConsolePrinterThread( server );
