c984a4a93a5f04af23f2ccf802e95ef679815161
Pierre-Arnaud Marcelot <pamarcelot@apache.org>
2013-01-11 17:47:11 +0000

    Externalized and translated strings.
    git-svn-id: https://svn.apache.org/repos/asf/directory/studio/trunk@1432190 13f79535-47bb-0310-9956-ffa450edef68

--- plugins/ldapservers.apacheds.v157/src/main/java/org/apache/directory/studio/ldapservers/apacheds/v157/ApacheDS157LdapServerAdapter.java
+++ plugins/ldapservers.apacheds.v157/src/main/java/org/apache/directory/studio/ldapservers/apacheds/v157/ApacheDS157LdapServerAdapter.java
@@ -101,10 +101,10 @@ public class ApacheDS157LdapServerAdapter implements LdapServerAdapter
 
         // Verifying and copying ApacheDS 1.5.7 libraries
         LdapServersUtils.verifyAndCopyLibraries( bundle, new Path( RESOURCES ).append( LIBS ),
-            getServerLibrariesFolder(), libraries, monitor, "verifying and copying ApacheDS 1.5.7 libraries" );
+            getServerLibrariesFolder(), libraries, monitor, Messages.getString("ApacheDS157LdapServerAdapter.VerifyingAndCopyingLibraries") ); //$NON-NLS-1$
 
         // Creating server folder structure
-        monitor.subTask( "creating server folder structure" );
+        monitor.subTask( Messages.getString("ApacheDS157LdapServerAdapter.CreatingServerFolderStructure") ); //$NON-NLS-1$
         File serverFolder = LdapServersManager.getServerFolder( server ).toFile();
         File confFolder = new File( serverFolder, CONF );
         confFolder.mkdir();
@@ -116,7 +116,7 @@ public class ApacheDS157LdapServerAdapter implements LdapServerAdapter
         partitionFolder.mkdir();
 
         // Copying configuration files
-        monitor.subTask( "copying configuration files" );
+        monitor.subTask( Messages.getString("ApacheDS157LdapServerAdapter.CopyingConfigurationFiles") ); //$NON-NLS-1$
         IPath resourceConfFolderPath = new Path( RESOURCES ).append( CONF );
         LdapServersUtils.copyResource( bundle, resourceConfFolderPath.append( SERVER_XML ), new File( confFolder,
             SERVER_XML ) );
@@ -174,7 +174,7 @@ public class ApacheDS157LdapServerAdapter implements LdapServerAdapter
 
         // Verifying and copying ApacheDS 1.5.7 libraries
         LdapServersUtils.verifyAndCopyLibraries( bundle, new Path( RESOURCES ).append( LIBS ),
-            getServerLibrariesFolder(), libraries, monitor, "verifying and copying ApacheDS 1.5.7 libraries" );
+            getServerLibrariesFolder(), libraries, monitor, Messages.getString("ApacheDS157LdapServerAdapter.VerifyingAndCopyingLibraries") ); //$NON-NLS-1$
 
         // Starting the console printer thread
         LdapServersUtils.startConsolePrinterThread( server );
