93ec5ab4511b5fd676c1e43ec0d1ee9719e40f8d
Stefan Seelmann <seelmann@apache.org>
2015-04-03 12:01:55 +0000

    Run 2nd step only if 1st succeeded
    git-svn-id: https://svn.apache.org/repos/asf/directory/studio/trunk@1671039 13f79535-47bb-0310-9956-ffa450edef68

--- build.sh
+++ build.sh
@@ -18,5 +18,4 @@
 
 # This script do a full build of Studio (including the MANIFEST generation and the P2 local repository construction)
 
-mvn -f pom-first.xml clean install
-mvn clean install
+mvn -f pom-first.xml clean install && mvn clean install
