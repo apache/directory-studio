118993e6b5248f8f1adecd6f5f399608d141c439
Emmanuel Lecharny <elecharny@apache.org>
2015-04-08 13:45:07 +0000

    o Added the two missing envFlags and maxEntrySize attributes. o Expose those attributes only when they are present in the schema.
    git-svn-id: https://svn.apache.org/repos/asf/directory/studio/trunk@1672089 13f79535-47bb-0310-9956-ffa450edef68

--- plugins/openldap.config.editor/src/main/java/org/apache/directory/studio/openldap/config/editor/databases/MdbDatabaseSpecificDetailsBlock.java
+++ plugins/openldap.config.editor/src/main/java/org/apache/directory/studio/openldap/config/editor/databases/MdbDatabaseSpecificDetailsBlock.java
@@ -22,6 +22,8 @@ package org.apache.directory.studio.openldap.config.editor.databases;
 
 import org.apache.directory.api.util.Strings;
 import org.apache.directory.studio.ldapbrowser.core.model.IBrowserConnection;
+import org.apache.directory.studio.openldap.config.model.database.OlcMdbConfig;
+import org.apache.directory.studio.openldap.config.model.widgets.IndicesWidget;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.VerifyEvent;
 import org.eclipse.swt.events.VerifyListener;
@@ -31,29 +33,79 @@ import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.forms.widgets.FormToolkit;
 import org.eclipse.ui.forms.widgets.Section;
-import org.apache.directory.studio.openldap.common.ui.widgets.BooleanWithDefaultWidget;
-import org.apache.directory.studio.openldap.common.ui.widgets.DirectoryBrowserWidget;
-import org.apache.directory.studio.openldap.common.ui.widgets.UnixPermissionsWidget;
-import org.apache.directory.studio.openldap.config.model.database.OlcMdbConfig;
-import org.apache.directory.studio.openldap.config.model.widgets.IndicesWidget;
 
 
 /**
- * This class implements a block for Memory-Mapped DB Specific Details.
+ * This class implements a block for Memory-Mapped DB Specific Details. The GUI will
+ * look like :
+ * 
+ * <pre>
+ * .--------------------------------------------------------------------.
+ * | Database Specific Settings                                         |
+ * +--------------------------------------------------------------------+
+ * | v Database Configuration                                           |
+ * |  Directory : [                                     [v] (browse...) |
+ * |  Mode :      [--------(0000)               ] (Edit Permissions...) |
+ * |                                                                    |
+ * | v Database indices                                                 |
+ * |  +----------------------------------------------+                  |
+ * |  | indice 1                                     | (Add)            |
+ * |  | indice 2                                     | (Edit)           |
+ * |  | ...                                          | (Delete)         |
+ * |  +----------------------------------------------+                  |
+ * |                                                                    |
+ * | v Database Limits                                                  |
+ * |  Maximum Readers :     [                         ]                 |
+ * |  Maximum Size :        [                         ]                 |
+ * |  Maximum Entry Size :  [                         ]                 | (2.4.41)
+ * |  Search Stack Depth :  [                         ]                 |
+ * |  Checkpoint Interval : [                         ]                 |
+ * |                                                                    |
+ * | v Database Options                                                 |
+ * |  Disable Synchronous Database Writes : [----------]                |
+ * |  Environment Flags :                                               | (2.4.33)
+ * |    +----------------------------------------------+                |
+ * |    | Flag 1                                       | (Add)          |
+ * |    | Flag 2                                       | (Edit)         |
+ * |    | ...                                          | (Delete)       |
+ * |    +----------------------------------------------+                |
+ * +--------------------------------------------------------------------+
+ * </pre>
  * 
  * @author <a href="mailto:dev@directory.apache.org">Apache Directory Project</a>
  */
 public class MdbDatabaseSpecificDetailsBlock extends AbstractDatabaseSpecificDetailsBlock<OlcMdbConfig>
 {
     // UI Widgets
+    /** The olcDbDirectory attribute (String) */
     private DirectoryBrowserWidget directoryBrowserWidget;
-    private UnixPermissionsWidget modeUnixPermissionsWidget;
-    private IndicesWidget indicesWidget;
-    private Text maxReadersText;
-    private Text maxSizeText;
-    private Text searchStackDepthText;
+
+    /** The olcDbCheckpoint attribute (String) */
     private Text checkpointText;
+
+    /** The olcDbEnvFlags attribute (String, multi-values) */
+    private Text envFlagsText;
+
+    /** The olcDbIndex attribute (String, multi-values) */
+    private IndicesWidget indicesWidget;
+
+    /** The olcMaxEntrySize attribute (Integer) No yet available (2.4.41) */
+    private Text maxEntrySizeText;
+
+    /** The olcDbMaxReaders attribute (Integer) */
+    private Text maxReadersText;
+
+    /** The olcMaxSize attribute (Long) */
+    private Text maxSizeText;
+
+    /** The olcDbMode attribute (String) */
+    private UnixPermissionsWidget modeUnixPermissionsWidget;
+
+    /** The olcDbNoSync attribute (Boolean) */
     private BooleanWithDefaultWidget disableSynchronousDatabaseWritesBooleanWithDefaultWidget;
+
+    /** The olcDbSearchStack attribute( Integer) */
+    private Text searchStackDepthText;
 
 
     /**
@@ -180,6 +232,14 @@ public class MdbDatabaseSpecificDetailsBlock extends AbstractDatabaseSpecificDet
         maxSizeText = createIntegerText( toolkit, databaseLimitsComposite );
         maxSizeText.setLayoutData( new GridData( SWT.FILL, SWT.NONE, true, false ) );
 
+        if ( browserConnection.getSchema().hasAttributeTypeDescription( "olcDbMaxEntrySize" ) )
+        {
+            // Max Entry Size Text
+            toolkit.createLabel( databaseLimitsComposite, "Maximum Entry Size:" );
+            maxEntrySizeText = createIntegerText( toolkit, databaseLimitsComposite );
+            maxEntrySizeText.setLayoutData( new GridData( SWT.FILL, SWT.NONE, true, false ) );
+        }
+
         // Search Stack Depth Text
         toolkit.createLabel( databaseLimitsComposite, "Search Stack Depth:" );
         searchStackDepthText = createIntegerText( toolkit, databaseLimitsComposite );
@@ -215,6 +275,8 @@ public class MdbDatabaseSpecificDetailsBlock extends AbstractDatabaseSpecificDet
         disableSynchronousDatabaseWritesBooleanWithDefaultWidget.create( databaseOptionsComposite, toolkit );
         disableSynchronousDatabaseWritesBooleanWithDefaultWidget.getControl().setLayoutData(
             new GridData( SWT.FILL, SWT.NONE, true, false ) );
+
+        // Env flags here...
     }
 
 
@@ -240,15 +302,15 @@ public class MdbDatabaseSpecificDetailsBlock extends AbstractDatabaseSpecificDet
 
             // Max Readers Text
             Integer maxReaders = database.getOlcDbMaxReaders();
-            maxReadersText.setText( ( maxReaders == null ) ? "" : "" + maxReaders ); //$NON-NLS-1$
+            maxReadersText.setText( ( maxReaders == null ) ? "" : maxReaders.toString() ); //$NON-NLS-1$
 
             // Max Size Text
             Long maxSize = database.getOlcDbMaxSize();
-            maxSizeText.setText( ( maxSize == null ) ? "" : "" + maxSize ); //$NON-NLS-1$
+            maxSizeText.setText( ( maxSize == null ) ? "" : maxSize.toString() ); //$NON-NLS-1$
 
             // Search Stack Depth Text
             Integer searchStackDepth = database.getOlcDbSearchStack();
-            searchStackDepthText.setText( ( searchStackDepth == null ) ? "" : "" + searchStackDepth ); //$NON-NLS-1$
+            searchStackDepthText.setText( ( searchStackDepth == null ) ? "" : searchStackDepth.toString() ); //$NON-NLS-1$
 
             // Checkpoint Text
             String checkpoint = database.getOlcDbCheckpoint();
@@ -274,6 +336,12 @@ public class MdbDatabaseSpecificDetailsBlock extends AbstractDatabaseSpecificDet
 
         maxReadersText.addModifyListener( dirtyModifyListener );
         maxSizeText.addModifyListener( dirtyModifyListener );
+
+        if ( browserConnection.getSchema().hasAttributeTypeDescription( "olcDbMaxEntrySize" ) )
+        {
+            maxEntrySizeText.addModifyListener( dirtyModifyListener );
+        }
+
         searchStackDepthText.addModifyListener( dirtyModifyListener );
         checkpointText.addModifyListener( dirtyModifyListener );
 
@@ -293,6 +361,12 @@ public class MdbDatabaseSpecificDetailsBlock extends AbstractDatabaseSpecificDet
 
         maxReadersText.removeModifyListener( dirtyModifyListener );
         maxSizeText.removeModifyListener( dirtyModifyListener );
+
+        if ( browserConnection.getSchema().hasAttributeTypeDescription( "olcDbMaxEntrySize" ) )
+        {
+            maxEntrySizeText.removeModifyListener( dirtyModifyListener );
+        }
+
         searchStackDepthText.removeModifyListener( dirtyModifyListener );
         checkpointText.removeModifyListener( dirtyModifyListener );
 
@@ -330,7 +404,7 @@ public class MdbDatabaseSpecificDetailsBlock extends AbstractDatabaseSpecificDet
      */
     public void commit( boolean onSave )
     {
-        // Directory Text
+        // Directory
         String directory = directoryBrowserWidget.getDirectoryPath();
 
         if ( Strings.isEmpty( directory ) )
@@ -344,17 +418,18 @@ public class MdbDatabaseSpecificDetailsBlock extends AbstractDatabaseSpecificDet
 
         directoryBrowserWidget.saveDialogSettings();
 
-        // Mode Text
+        // Mode
         database.setOlcDbMode( modeUnixPermissionsWidget.getValue() );
 
-        // Indices Widget
+        // Indices
         database.clearOlcDbIndex();
+
         for ( String index : indicesWidget.getIndices() )
         {
             database.addOlcDbIndex( index );
         }
 
-        // Cache Size Text
+        // Max readers
         try
         {
             database.setOlcDbMaxReaders( Integer.parseInt( maxReadersText.getText() ) );
@@ -364,7 +439,7 @@ public class MdbDatabaseSpecificDetailsBlock extends AbstractDatabaseSpecificDet
             database.setOlcDbMaxReaders( null );
         }
 
-        // Cache Free Text
+        // Max Size
         try
         {
             database.setOlcDbMaxSize( Long.parseLong( maxSizeText.getText() ) );
@@ -374,7 +449,20 @@ public class MdbDatabaseSpecificDetailsBlock extends AbstractDatabaseSpecificDet
             database.setOlcDbMaxSize( null );
         }
 
-        // Search Stack Depth Text
+        // Max Entry Size
+        if ( browserConnection.getSchema().hasAttributeTypeDescription( "olcDbMaxEntrySize" ) )
+        {
+            try
+            {
+                database.setOlcDbMaxEntrySize( Integer.parseInt( maxEntrySizeText.getText() ) );
+            }
+            catch ( NumberFormatException e )
+            {
+                database.setOlcDbMaxEntrySize( null );
+            }
+        }
+
+        // Search Stack Depth
         try
         {
             database.setOlcDbSearchStack( Integer.parseInt( searchStackDepthText.getText() ) );
@@ -384,10 +472,10 @@ public class MdbDatabaseSpecificDetailsBlock extends AbstractDatabaseSpecificDet
             database.setOlcDbSearchStack( null );
         }
 
-        // Checkpoint Interval Text
+        // Checkpoint Interval
         database.setOlcDbCheckpoint( checkpointText.getText() );
 
-        // Disable Synchronous Database Writes Widget
+        // Disable Synchronous Database Writes
         database.setOlcDbNoSync( disableSynchronousDatabaseWritesBooleanWithDefaultWidget.getValue() );
     }
 }
