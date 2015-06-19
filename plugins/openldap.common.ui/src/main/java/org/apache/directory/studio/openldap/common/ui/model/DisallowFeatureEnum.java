14e584c40c9e18bebaf6e8d06b8b9718dbc309a2
Emmanuel Lecharny <elecharny@apache.org>
2015-06-19 13:14:30 +0000

    Added the olcDisallows parameters
    git-svn-id: https://svn.apache.org/repos/asf/directory/studio/trunk@1686406 13f79535-47bb-0310-9956-ffa450edef68

---
+++ plugins/openldap.common.ui/src/main/java/org/apache/directory/studio/openldap/common/ui/model/DisallowFeatureEnum.java
@@ -0,0 +1,106 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *  
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License. 
+ *  
+ */
+package org.apache.directory.studio.openldap.common.ui.model;
+
+/**
+ * An enum for the various possible value of the olcDisallows parameter. One of
+ * <ul>
+ * <li>bind_anon</li>
+ * <li>bind_simple</li>
+ * <li>tls_2_anon</li>
+ * <li>tls_authc</li>
+ * <li>proxy_authz_non_critical</li>
+ * <li>dontusecopy_non_critical</li>
+ * </ul>
+ * 
+ * @author <a href="mailto:dev@directory.apache.org">Apache Directory Project</a>
+ */
+public enum DisallowFeatureEnum
+{
+    BIND_ANON( "bind_anon" ),
+    BIND_SIMPLE( "bind_simple" ),
+    TLS_2_ANON( "tls_2_anon" ),
+    TLS_AUTHC( "tls_authc" ),
+    PROXY_AUTHZ_NON_CRITICAL( "proxy_authz_non_critical" ),
+    DONTUSECOPY_NON_CRITICAL( "dontusecopy_non_critical" ),
+    UNKNOWN( "---" );
+    
+    /** The interned name */
+    private String name;
+    
+    /**
+     * A private constructor for this enum
+     */
+    private DisallowFeatureEnum( String name )
+    {
+        this.name = name;
+    }
+
+    
+    /**
+     * Return an instance of DisallowFeatureEnum from a String
+     * 
+     * @param name The feature's name
+     * @return The associated DisallowFeatureEnum
+     */
+    public static DisallowFeatureEnum getFeature( String name )
+    {
+        if ( BIND_ANON.name.equalsIgnoreCase( name ) )
+        {
+            return BIND_ANON;
+        }
+        
+        if ( BIND_SIMPLE.name.equalsIgnoreCase( name ) )
+        {
+            return BIND_SIMPLE;
+        }
+        
+        if ( DONTUSECOPY_NON_CRITICAL.name.equalsIgnoreCase( name ) )
+        {
+            return DONTUSECOPY_NON_CRITICAL;
+        }
+        
+        if ( PROXY_AUTHZ_NON_CRITICAL.name.equalsIgnoreCase( name ) )
+        {
+            return PROXY_AUTHZ_NON_CRITICAL;
+        }
+        
+        if ( TLS_2_ANON.name.equalsIgnoreCase( name ) )
+        {
+            return TLS_2_ANON;
+        }
+        
+        if ( TLS_AUTHC.name.equalsIgnoreCase( name ) )
+        {
+            return TLS_AUTHC;
+        }
+        
+        return UNKNOWN;
+    }
+    
+    
+    /**
+     * @return the name
+     */
+    public String getName()
+    {
+        return name;
+    }
+}
