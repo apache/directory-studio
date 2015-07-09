01b55a61b66f5ff972030e85aa70d480506c27fc
Emmanuel Lecharny <elecharny@apache.org>
2015-07-09 22:34:39 +0000

    Added enum for the olcLimits
    git-svn-id: https://svn.apache.org/repos/asf/directory/studio/trunk@1690181 13f79535-47bb-0310-9956-ffa450edef68

---
+++ plugins/openldap.common.ui/src/main/java/org/apache/directory/studio/openldap/common/ui/model/DnSpecTypeEnum.java
@@ -0,0 +1,72 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *  
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License. 
+ *  
+ */
+package org.apache.directory.studio.openldap.common.ui.model;
+
+
+/**
+ * An enumeration of all the possible olcLimits selector dnspec type
+ *
+ * @author <a href="mailto:dev@directory.apache.org">Apache Directory Project</a>
+ */
+public enum DnSpecTypeEnum
+{
+    SELF( "self" ),
+    THIS( "this" ),
+    NONE( "---" );
+    
+    /** The associated name */
+    private String name;
+    
+    /**
+     * Creates a dnspec type instance
+     */
+    private DnSpecTypeEnum( String name )
+    {
+        this.name = name;
+    }
+
+    /**
+     * @return the text
+     */
+    public String getName()
+    {
+        return name;
+    }
+    
+    
+    /**
+     * Retrieve the instance associated to a String. Return NONE if not found.
+     * 
+     * @param name The name to retrieve
+     * @return The DnSpecTypeEnum instance found, or NONE.
+     */
+    public static DnSpecTypeEnum getSelector( String name )
+    {
+        for ( DnSpecTypeEnum dnSpecType : values() )
+        {
+            if ( dnSpecType.name.equalsIgnoreCase( name ) )
+            {
+                return dnSpecType;
+            }
+        }
+        
+        return NONE;
+    }
+}
