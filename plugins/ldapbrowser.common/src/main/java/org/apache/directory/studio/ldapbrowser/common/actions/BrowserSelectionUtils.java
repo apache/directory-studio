0c346f326b8b651456e1a4409dd24ff78aeb5191
Emmanuel Lecharny <elecharny@apache.org>
2015-07-24 12:29:31 +0000

    o Don't use an iterator anymore, it's useless
    git-svn-id: https://svn.apache.org/repos/asf/directory/studio/trunk@1692495 13f79535-47bb-0310-9956-ffa450edef68

--- plugins/ldapbrowser.common/src/main/java/org/apache/directory/studio/ldapbrowser/common/actions/BrowserSelectionUtils.java
+++ plugins/ldapbrowser.common/src/main/java/org/apache/directory/studio/ldapbrowser/common/actions/BrowserSelectionUtils.java
@@ -386,15 +386,12 @@ public abstract class BrowserSelectionUtils extends SelectionUtils
         if ( selection instanceof IStructuredSelection )
         {
             IStructuredSelection structuredSelection = ( IStructuredSelection ) selection;
-            Iterator<?> it = structuredSelection.iterator();
             
-            while ( it.hasNext() )
+            for ( Object element : structuredSelection.toArray() )
             {
-                Object o = it.next();
-                
-                if ( type.isInstance( o ) )
+                if ( type.isInstance( element ) )
                 {
-                    list.add( o );
+                    list.add( element );
                 }
             }
         }
