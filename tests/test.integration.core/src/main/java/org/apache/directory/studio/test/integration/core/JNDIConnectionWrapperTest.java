b38364f254511c2d393dce3f8c35c07917f8bcfb
Stefan Seelmann <seelmann@apache.org>
2017-02-11 22:41:43 +0000

    DIRSTUDIO-1129: Add referral handling tests
    git-svn-id: https://svn.apache.org/repos/asf/directory/studio/trunk@1782643 13f79535-47bb-0310-9956-ffa450edef68

--- tests/test.integration.core/src/main/java/org/apache/directory/studio/test/integration/core/JNDIConnectionWrapperTest.java
+++ tests/test.integration.core/src/main/java/org/apache/directory/studio/test/integration/core/JNDIConnectionWrapperTest.java
@@ -22,6 +22,7 @@ package org.apache.directory.studio.test.integration.core;
 
 
 import static org.apache.directory.studio.test.integration.core.Constants.LOCALHOST;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
@@ -34,25 +35,37 @@ import javax.naming.AuthenticationException;
 import javax.naming.CommunicationException;
 import javax.naming.NamingEnumeration;
 import javax.naming.NamingException;
+import javax.naming.directory.Attributes;
+import javax.naming.directory.DirContext;
+import javax.naming.directory.ModificationItem;
 import javax.naming.directory.SearchControls;
 import javax.naming.directory.SearchResult;
 
+import org.apache.directory.api.ldap.model.entry.AttributeUtils;
+import org.apache.directory.api.ldap.model.entry.DefaultAttribute;
+import org.apache.directory.api.ldap.model.entry.DefaultEntry;
+import org.apache.directory.api.ldap.model.entry.Entry;
+import org.apache.directory.api.ldap.model.name.Dn;
 import org.apache.directory.server.annotations.CreateLdapServer;
 import org.apache.directory.server.annotations.CreateTransport;
+import org.apache.directory.server.core.annotations.ApplyLdifFiles;
 import org.apache.directory.server.core.integ.AbstractLdapTestUnit;
 import org.apache.directory.server.core.integ.FrameworkRunner;
 import org.apache.directory.studio.common.core.jobs.StudioProgressMonitor;
 import org.apache.directory.studio.connection.core.Connection;
+import org.apache.directory.studio.connection.core.ConnectionCorePlugin;
 import org.apache.directory.studio.connection.core.Connection.AliasDereferencingMethod;
 import org.apache.directory.studio.connection.core.Connection.ReferralHandlingMethod;
 import org.apache.directory.studio.connection.core.ConnectionParameter;
 import org.apache.directory.studio.connection.core.ConnectionParameter.AuthenticationMethod;
 import org.apache.directory.studio.connection.core.ConnectionParameter.EncryptionMethod;
 import org.apache.directory.studio.connection.core.ConnectionParameter.NetworkProvider;
+import org.apache.directory.studio.connection.core.IReferralHandler;
 import org.apache.directory.studio.connection.core.io.ConnectionWrapper;
 import org.apache.directory.studio.connection.core.io.jndi.JNDIConnectionWrapper;
 import org.apache.mina.util.AvailablePortFinder;
 import org.eclipse.core.runtime.NullProgressMonitor;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -65,10 +78,23 @@ import org.junit.runner.RunWith;
  */
 @RunWith(FrameworkRunner.class)
 @CreateLdapServer(transports =
-    { @CreateTransport(protocol = "LDAP"),
-        @CreateTransport(protocol = "LDAPS") })
+    { @CreateTransport(protocol = "LDAP"), @CreateTransport(protocol = "LDAPS") })
+@ApplyLdifFiles(clazz = JNDIConnectionWrapperTest.class, value = "org/apache/directory/studio/test/integration/core/TestData.ldif")
 public class JNDIConnectionWrapperTest extends AbstractLdapTestUnit
 {
+
+    @Before
+    public void setUp() throws Exception
+    {
+        // create referral entry
+        Entry entry = new DefaultEntry( getService().getSchemaManager() );
+        entry.setDn( new Dn( "cn=referral,ou=system" ) );
+        entry.add( "objectClass", "top", "referral", "extensibleObject" );
+        entry.add( "cn", "referral" );
+        entry.add( "ref", "ldap://" + LOCALHOST + ":" + ldapServer.getPort() + "/ou=users,ou=system" );
+        service.getAdminSession().add( entry );
+    }
+
 
     /**
      * Tests connecting to the server.
@@ -206,26 +232,97 @@ public class JNDIConnectionWrapperTest extends AbstractLdapTestUnit
     @Test
     public void testSearch()
     {
-        StudioProgressMonitor monitor = null;
-        ConnectionParameter connectionParameter = null;
-        Connection connection = null;
-        ConnectionWrapper connectionWrapper = null;
-
-        // simple auth without principal and credential
-        monitor = getProgressMonitor();
-        connectionParameter = new ConnectionParameter( null, LOCALHOST, ldapServer.getPort(), EncryptionMethod.NONE,
-            NetworkProvider.JNDI, AuthenticationMethod.SIMPLE, "uid=admin,ou=system", "secret", null, true, null );
-        connection = new Connection( connectionParameter );
-        connectionWrapper = connection.getConnectionWrapper();
-        connectionWrapper.connect( monitor );
-        connectionWrapper.bind( monitor );
-        assertTrue( connectionWrapper.isConnected() );
-        assertNull( monitor.getException() );
+        StudioProgressMonitor monitor = getProgressMonitor();
+        ConnectionWrapper connectionWrapper = getConnectionWrapper( monitor );
 
         SearchControls searchControls = new SearchControls();
-        NamingEnumeration<SearchResult> result = connectionWrapper.search( "ou=system", "objectClass=*",
-            searchControls, AliasDereferencingMethod.NEVER, ReferralHandlingMethod.IGNORE, null, monitor, null );
+        NamingEnumeration<SearchResult> result = connectionWrapper.search( "ou=system", "objectClass=*", searchControls,
+            AliasDereferencingMethod.NEVER, ReferralHandlingMethod.IGNORE, null, monitor, null );
         assertNotNull( result );
+    }
+
+
+    @Test
+    public void testSearchContinuation() throws NamingException
+    {
+        StudioProgressMonitor monitor = getProgressMonitor();
+        SearchControls searchControls = new SearchControls();
+        NamingEnumeration<SearchResult> result = getConnectionWrapper( monitor ).search( "cn=referral,ou=system",
+            "objectClass=*", searchControls, AliasDereferencingMethod.NEVER, ReferralHandlingMethod.FOLLOW, null,
+            monitor, null );
+
+        assertFalse( monitor.isCanceled() );
+        assertFalse( monitor.errorsReported() );
+        assertNotNull( result );
+        assertTrue( result.hasMore() );
+        SearchResult entry = result.next();
+        assertNotNull( entry );
+        assertEquals( "uid=user.1,ou=users,ou=system", entry.getNameInNamespace() );
+    }
+
+
+    @Test
+    public void testAddFollowsReferral() throws Exception
+    {
+        String targetDn = "uid=user.X,ou=users,ou=system";
+        String referralDn = "uid=user.X,cn=referral,ou=system";
+
+        // create entry under referral
+        StudioProgressMonitor monitor = getProgressMonitor();
+        Attributes attributes = AttributeUtils.toAttributes(
+            new DefaultEntry( referralDn, "objectClass: inetOrgPerson", "sn: X", "cn: X", "uid: user.X" ) );
+        getConnectionWrapper( monitor ).createEntry( referralDn, attributes, null, monitor, null );
+
+        // should have created target entry
+        assertFalse( monitor.isCanceled() );
+        assertFalse( monitor.errorsReported() );
+        assertTrue( service.getAdminSession().exists( targetDn ) );
+    }
+
+
+    @Test
+    public void testModifyFollowsReferral() throws Exception
+    {
+        String targetDn = "uid=user.X,ou=users,ou=system";
+        String referralDn = "uid=user.X,cn=referral,ou=system";
+
+        // create target entry
+        service.getAdminSession().add( new DefaultEntry( service.getSchemaManager(), targetDn,
+            "objectClass: inetOrgPerson", "sn: X", "cn: X", "uid: user.X" ) );
+
+        // modify referral entry 
+        StudioProgressMonitor monitor = getProgressMonitor();
+        ModificationItem[] modificationItems =
+            { new ModificationItem( DirContext.REPLACE_ATTRIBUTE,
+                AttributeUtils.toJndiAttribute( new DefaultAttribute( "sn", "modified" ) ) ) };
+        getConnectionWrapper( monitor ).modifyEntry( referralDn, modificationItems, null, monitor, null );
+
+        // should have modified the target entry
+        assertFalse( monitor.isCanceled() );
+        assertFalse( monitor.errorsReported() );
+        Entry entry = service.getAdminSession().lookup( new Dn( targetDn ) );
+        assertEquals( "modified", entry.get( "sn" ).getString() );
+    }
+
+
+    @Test
+    public void testDeleteFollowsReferral() throws Exception
+    {
+        String targetDn = "uid=user.X,ou=users,ou=system";
+        String referralDn = "uid=user.X,cn=referral,ou=system";
+
+        // create target entry
+        service.getAdminSession().add( new DefaultEntry( service.getSchemaManager(), targetDn,
+            "objectClass: inetOrgPerson", "sn: X", "cn: X", "uid: user.X" ) );
+
+        // delete referral entry 
+        StudioProgressMonitor monitor = getProgressMonitor();
+        getConnectionWrapper( monitor ).deleteEntry( referralDn, null, monitor, null );
+
+        // should have deleted the target entry
+        assertFalse( monitor.isCanceled() );
+        assertFalse( monitor.errorsReported() );
+        assertFalse( service.getAdminSession().exists( targetDn ) );
     }
 
 
@@ -233,6 +330,31 @@ public class JNDIConnectionWrapperTest extends AbstractLdapTestUnit
     {
         StudioProgressMonitor monitor = new StudioProgressMonitor( new NullProgressMonitor() );
         return monitor;
+    }
+
+
+    private ConnectionWrapper getConnectionWrapper( StudioProgressMonitor monitor )
+    {
+        // simple auth without principal and credential
+        ConnectionParameter connectionParameter = new ConnectionParameter( null, LOCALHOST, ldapServer.getPort(),
+            EncryptionMethod.NONE, NetworkProvider.JNDI, AuthenticationMethod.SIMPLE, "uid=admin,ou=system", "secret",
+            null, false, null );
+
+        Connection connection = new Connection( connectionParameter );
+
+        ConnectionWrapper connectionWrapper = connection.getConnectionWrapper();
+        connectionWrapper.connect( monitor );
+        connectionWrapper.bind( monitor );
+
+        IReferralHandler referralHandler = referralUrls -> {
+            return connection;
+        };
+        ConnectionCorePlugin.getDefault().setReferralHandler( referralHandler );
+
+        assertTrue( connectionWrapper.isConnected() );
+        assertNull( monitor.getException() );
+
+        return connectionWrapper;
     }
 
 }
