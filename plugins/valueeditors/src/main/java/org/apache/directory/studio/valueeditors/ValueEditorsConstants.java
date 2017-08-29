2f6cc3f27b4b1290ab87b986d1ec7f626539f2dc
Emmanuel Lécharny <elecharny@apache.org>
2017-08-29 17:02:35 +0000

    o Get rid of Plugin_id in plugin.properties o Removed the various ID defined in plugin.properties that are used only once
    git-svn-id: https://svn.apache.org/repos/asf/directory/studio/branches/studio-value@1806621 13f79535-47bb-0310-9956-ffa450edef68

--- plugins/valueeditors/src/main/java/org/apache/directory/studio/valueeditors/ValueEditorsConstants.java
+++ plugins/valueeditors/src/main/java/org/apache/directory/studio/valueeditors/ValueEditorsConstants.java
@@ -38,8 +38,7 @@ public final class ValueEditorsConstants
     }
 
     /** The plug-in ID */
-    public static final String PLUGIN_ID = ValueEditorsActivator.getDefault().getPluginProperties()
-        .getString( "Plugin_id" ); //$NON-NLS-1$
+    public static final String PLUGIN_ID = ValueEditorsConstants.class.getPackage().getName();
 
     /** The relative path to the image editor icon */
     public static final String IMG_IMAGEEDITOR = "resources/icons/imageeditor.gif"; //$NON-NLS-1$
